95c40a6ba4d01a48b1b0a891ceb9e0c88489f2b5
Axel Huebl <axel.huebl@plasma.ninja>
2018-02-19 00:49:12 +0100

    Python: Datatype, Iteration+Enc

--- src/binding/python/openPMD.cpp
+++ src/binding/python/openPMD.cpp
@@ -6,10 +6,10 @@ namespace py = pybind11;
 // forward declarations of exposed classes
 void init_Series(py::module &);
 void init_Dataset(py::module &);
-/*
 void init_Datatype(py::module &);
 void init_Iteration(py::module &);
 void init_IterationEncoding(py::module &);
+/*
 void init_Mesh(py::module &);
 void init_ParticlePatches(py::module &);
 void init_ParticleSpecies(py::module &);
@@ -21,18 +21,18 @@ void init_RecordComponent(py::module &);
 PYBIND11_MODULE(openPMD, m) {
     // m.doc() = ...;
 
-    init_Series(m);
     init_Dataset(m);
-    /*
     init_Datatype(m);
     init_Iteration(m);
     init_IterationEncoding(m);
+    /*
     init_Mesh(m);
     init_ParticlePatches(m);
     init_ParticleSpecies(m);
     init_Record(m);
     init_RecordComponent(m);
     */
+    init_Series(m);
 
     // m.attr("__version__") = "1.2.3-dev";
 }
