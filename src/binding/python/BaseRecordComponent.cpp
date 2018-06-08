2b0b1bcdf2afeadd4e1383b3f429e52bb6753e7d
Axel Huebl <axel.huebl@plasma.ninja>
2018-06-08 18:08:43 +0200

    Python: RecordComponent dtype numpy
    numpy-ize the `.dtype` of our record components.

--- src/binding/python/BaseRecordComponent.cpp
+++ src/binding/python/BaseRecordComponent.cpp
@@ -19,9 +19,10 @@
  * If not, see <http://www.gnu.org/licenses/>.
  */
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 
 #include "openPMD/backend/BaseRecordComponent.hpp"
-//include "openPMD/backend/Attributable.hpp"
+#include "openPMD/Datatype.hpp"
 
 #include <sstream>
 
@@ -30,7 +31,6 @@ using namespace openPMD;
 
 
 void init_BaseRecordComponent(py::module &m) {
-    //                            , Attributable
     py::class_<BaseRecordComponent>(m, "Base_Record_Component")
         .def("__repr__",
             [](BaseRecordComponent const & brc) {
@@ -44,6 +44,63 @@ void init_BaseRecordComponent(py::module &m) {
         .def("reset_datatype", &BaseRecordComponent::resetDatatype)
 
         .def_property_readonly("unit_SI", &BaseRecordComponent::unitSI)
-        .def_property_readonly("dtype", &BaseRecordComponent::getDatatype)
+        .def_property_readonly("dtype", [](BaseRecordComponent & brc) {
+            using DT = Datatype;
+
+            if( brc.getDatatype() == DT::CHAR )
+                return py::dtype("b");
+            else if( brc.getDatatype() == DT::UCHAR )
+                return py::dtype("B");
+            else if( brc.getDatatype() == DT::INT16 )
+                return py::dtype("int16");
+            else if( brc.getDatatype() == DT::INT32 )
+                return py::dtype("int32");
+            else if( brc.getDatatype() == DT::INT64 )
+                return py::dtype("int64");
+            else if( brc.getDatatype() == DT::UINT16 )
+                return py::dtype("uint16");
+            else if( brc.getDatatype() == DT::UINT32 )
+                return py::dtype("uint32");
+            else if( brc.getDatatype() == DT::UINT64 )
+                return py::dtype("uint64");
+            else if( brc.getDatatype() == DT::LONG_DOUBLE )
+                return py::dtype("longdouble");
+            else if( brc.getDatatype() == DT::DOUBLE )
+                return py::dtype("double");
+            else if( brc.getDatatype() == DT::FLOAT )
+                return py::dtype("float");
+            /*
+            else if( brc.getDatatype() == DT::STRING )
+                return py::dtype("string_");
+            else if( brc.getDatatype() == DT::VEC_CHAR )
+                return py::dtype("");
+            else if( brc.getDatatype() == DT::VEC_INT16 )
+                return py::dtype("");
+            else if( brc.getDatatype() == DT::VEC_INT32 )
+                return py::dtype("");
+            else if( brc.getDatatype() == DT::VEC_INT64 )
+                return py::dtype("");
+            else if( brc.getDatatype() == DT::VEC_UCHAR )
+                return py::dtype("");
+            else if( brc.getDatatype() == DT::VEC_UINT16 )
+                return py::dtype("");
+            else if( brc.getDatatype() == DT::VEC_UINT32 )
+                return py::dtype("");
+            else if( brc.getDatatype() == DT::VEC_UINT64 )
+                return py::dtype("");
+            else if( brc.getDatatype() == DT::ARR_DBL_7 )
+                return py::dtype("");
+            else if( brc.getDatatype() == DT::VEC_LONG_DOUBLE )
+                return py::dtype("");
+            else if( brc.getDatatype() == DT::VEC_DOUBLE )
+                return py::dtype("");
+            else if( brc.getDatatype() == DT::VEC_FLOAT )
+                return py::dtype("");
+            else if( brc.getDatatype() == DT::VEC_STRING )
+                return py::dtype("");
+            */
+            else
+                return py::dtype("void");
+        })
     ;
 }
