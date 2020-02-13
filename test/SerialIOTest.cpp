cc5600d029fa0d5777abd22dff997e5e9e8912da
Junmin Gu <jgu@lbl.gov>
2020-02-13 07:41:39 -0800

    put back the flush that I commented out from original code

--- test/SerialIOTest.cpp
+++ test/SerialIOTest.cpp
@@ -724,7 +724,7 @@ void fileBased_write_test(const std::string & backend)
         o.setOpenPMDextension(1);
         o.iterations[3].setTime(static_cast< double >(3));
         o.iterations[4].setTime(static_cast< double >(4));
-        //o.flush();
+        o.flush();
         o.iterations[5].setTime(static_cast< double >(5));
     }
     REQUIRE((auxiliary::file_exists("../samples/subdir/serial_fileBased_write00000001." + backend)
