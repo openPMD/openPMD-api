5295b4e3f68c6a548cb66da9989622930ac21c92
Axel Huebl <axel.huebl@plasma.ninja>
2020-03-05 22:13:08 -0800

    Post 0.11.0-alpha Changelog Template
    Change back to `-dev` versions after 0.11.0 release :)

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -3,6 +3,27 @@
 Changelog
 =========
 
+0.11.1-alpha
+------------
+**Date:** TBA
+
+[Title]
+
+[Summary]
+
+Changes to "0.11.0-alpha"
+^^^^^^^^^^^^^^^^^^^^^^^^
+
+Features
+""""""""
+
+Bug Fixes
+"""""""""
+
+Other
+"""""
+
+
 0.11.0-alpha
 ------------
 **Date:** 2020-03-05
--- include/openPMD/version.hpp
+++ include/openPMD/version.hpp
@@ -28,8 +28,8 @@
  */
 #define OPENPMDAPI_VERSION_MAJOR 0
 #define OPENPMDAPI_VERSION_MINOR 11
-#define OPENPMDAPI_VERSION_PATCH 0
-#define OPENPMDAPI_VERSION_LABEL "alpha"
+#define OPENPMDAPI_VERSION_PATCH 1
+#define OPENPMDAPI_VERSION_LABEL "dev"
 /** @} */
 
 /** maximum supported version of the openPMD standard (read & write, compile-time)
