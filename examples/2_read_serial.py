78f6327a58d61210a4f8327b76c05fe561a03886
LDAmorim <LDianaAmorim@lbl.gov>
2020-03-26 11:14:21 -0700

    Added whitespace after comma (not sure it fixes issue)

--- examples/2_read_serial.py
+++ examples/2_read_serial.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     s_e=[ps for ps in i.particles][0]
     s_c=[r for r in i.particles[s_e]][0]
     charge=i.particles[s_e][s_c][openpmd_api.Mesh_Record_Component.SCALAR][0]
-    print("And ", s_c, " = ",charge)
+    print("And ", s_c, " = ", charge)
     print("")
 
     E_x = i.meshes["E"]["x"]
