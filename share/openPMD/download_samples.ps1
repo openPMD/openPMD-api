dc8fbadc0e55ac299938fb593e91b9fcbd96c678
Axel Huebl <axel.huebl@plasma.ninja>
2020-04-18 23:05:45 -0700

    fix appveyor / PS RemoveItem Syntax
    somehow this changed...

--- share/openPMD/download_samples.ps1
+++ share/openPMD/download_samples.ps1
@@ -7,7 +7,8 @@ Invoke-WebRequest https://github.com/openPMD/openPMD-example-datasets/raw/draft/
 7z.exe x -r example-thetaMode.tar
 Move-Item -Path example-3d\hdf5\* samples\git-sample\
 Move-Item -Path example-thetaMode\hdf5\* samples\git-sample\thetaMode/
-Remove-Item -Recurse -Force example-3d* example-thetaMode*
+Remove-Item -Recurse -Force example-3d*
+Remove-Item -Recurse -Force example-thetaMode*
 
 # Ref.: https://github.com/yt-project/yt/pull/1645
 New-item -ItemType directory -Name samples\issue-sample\
