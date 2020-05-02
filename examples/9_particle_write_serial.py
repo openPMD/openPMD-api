Examples: Fix ED-PIC "weighting"
The ED-PIC extension defines a "weighting". Its attributes are fixed
and the current example is confusing (and
extension/standard-violating).

examples/9_particle_write_serial.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     # don't like it anymore? remove it with:
     # del electrons["displacement"]
 
-    electrons["weighting"][SCALAR].set_unit_SI(1.e-5)
+    electrons["weighting"][SCALAR].make_constant(1.e-5)
 
     particlePos_x = np.random.rand(234).astype(np.float32)
     particlePos_y = np.random.rand(234).astype(np.float32)