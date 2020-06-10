b704ad6729ddb390664508e7f777fbc6e1ab5325
guj <jgu@lbl.gov>
2020-06-09 18:08:39 -0700

    using sync mode to write out rather than deferred, to reduce memory consumption

--- src/IO/ADIOS/ADIOS2IOHandler.cpp
+++ src/IO/ADIOS/ADIOS2IOHandler.cpp
@@ -1126,7 +1126,9 @@ namespace detail
         adios2::Variable< T > var = m_impl->verifyDataset< T >(
             bp.param.offset, bp.param.extent, IO, bp.name );
 
-        engine.Put( var, ptr );
+	// using sync mode has the same effect as deferred and less memory consumption
+	// at of now.
+        engine.Put( var, ptr, adios2::Mode::Sync );
     }
 
     template < typename T >
