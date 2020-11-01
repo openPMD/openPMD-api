241196af920a5cf2559faa25eed76e15c9769d44
guj <guj@users.noreply.github.com>
2020-11-01 00:21:19 -0700

    added tests  8a & 8b to do 1D/2D mesh writing and reading (#803)
    * added tests  8a & 8b to do 1D/2D mesh writing and reading
    
    * tabs fix
    
    * space fix
    
    * added comments and defaults
    
    * style fix
    
    * more style fix
    
    * added position/offset for validation requirement
    
    * fixed a typo plus format change
    
    * fixed a bug & fixed typos
    
    * tab
    
    * eol fix
    
    * Update examples/8a_benchmark_write1D2D_parallel.cpp
    
    style change by Axel
    
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>
    
    * Update examples/8a_benchmark_write1D2D_parallel.cpp
    
    style change by Axel..
    
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>
    
    * Update examples/8a_benchmark_write1D2D_parallel.cpp
    
    style change by Axel
    
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>
    
    * Update examples/8b_benchmark_read1D2D_parallel.cpp
    
    style change by Axel ...
    
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>
    
    * more comments
    removed const char*
    
    * replaced char* with string
    
    * eol
    
    * Cleanup Benchmarks
    
    Deduplicate auxiliary code, fix formatting, fix doxygen, try to
    document undocumented parameters.
    
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>

---
+++ include/openPMD/benchmark/MemoryProfiler.hpp
@@ -0,0 +1,91 @@
+/* Copyright 2020 Junmin Gu
+ *
+ * This file is part of openPMD-api.
+ *
+ * openPMD-api is free software: you can redistribute it and/or modify
+ * it under the terms of of either the GNU General Public License or
+ * the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * openPMD-api is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with openPMD-api.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <chrono>
+#include <fstream>
+#include <iostream>
+
+
+namespace openPMD
+{
+namespace benchmark
+{
+    /** The Memory profiler class for profiling purpose
+     *
+     *  Simple Memory usage report that works on linux system
+     */
+    class MemoryProfiler
+    {
+    public:
+        /** Simple Memory profiler for linux
+         *
+         * @param[in] rank     MPI rank
+         * @param[in] tag      item name to measure
+         */
+        MemoryProfiler( int rank, const std::string& tag )
+        : m_Rank( rank ), m_Name( "" )
+        {
+#if defined(__linux)
+            //m_Name = "/proc/meminfo";
+            m_Name = "/proc/self/status";
+            Display( tag );
+#endif
+        }
+
+        /** Display virtual memory info
+         *
+         * Read from /proc/self/status and display the Virtual Memory info at
+         * rank 0 to stdout
+         *
+         * @param tag      item name to measure
+         */
+        void Display(const std::string& tag){
+            if (0 == m_Name.size())
+                return;
+
+            if (m_Rank > 0)
+                return;
+
+            std::cout<<" memory at:  "<<tag;
+            std::ifstream input(m_Name.c_str());
+
+            if (input.is_open()) {
+                for (std::string line; getline(input, line);)
+                {
+                    if (line.find("VmRSS") == 0)
+                        std::cout<<line<<" ";
+                    if (line.find("VmSize") == 0)
+                        std::cout<<line<<" ";
+                    if (line.find("VmSwap") == 0)
+                        std::cout<<line;
+                }
+                std::cout<<std::endl;
+                input.close();
+            }
+        }
+    private:
+        int m_Rank;
+        std::string m_Name;
+    };
+}
+}
