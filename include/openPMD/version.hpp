23e8dfd1028a8ff6c287b271cd59b36b14bc5f5f
Axel Huebl <axel.huebl@plasma.ninja>
2021-01-08 13:43:01 -0800

    Release: 0.13.1 (#891)
    Update version and write announcement text in changelog.

--- include/openPMD/version.hpp
+++ include/openPMD/version.hpp
@@ -30,7 +30,7 @@
 #define OPENPMDAPI_VERSION_MAJOR 0
 #define OPENPMDAPI_VERSION_MINOR 13
 #define OPENPMDAPI_VERSION_PATCH 1
-#define OPENPMDAPI_VERSION_LABEL "dev"
+#define OPENPMDAPI_VERSION_LABEL ""
 /** @} */
 
 /** maximum supported version of the openPMD standard (read & write, compile-time)
