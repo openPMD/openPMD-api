708da9b35f8c63638304368b55550844e4839a8a
Axel Huebl <axel.huebl@plasma.ninja>
2021-01-08 13:46:59 -0800

    Post 0.13.1 Changelog Template
    Change back to `-dev` after 0.13.1 release :)

--- include/openPMD/version.hpp
+++ include/openPMD/version.hpp
@@ -29,8 +29,8 @@
  */
 #define OPENPMDAPI_VERSION_MAJOR 0
 #define OPENPMDAPI_VERSION_MINOR 13
-#define OPENPMDAPI_VERSION_PATCH 1
-#define OPENPMDAPI_VERSION_LABEL ""
+#define OPENPMDAPI_VERSION_PATCH 2
+#define OPENPMDAPI_VERSION_LABEL "dev"
 /** @} */
 
 /** maximum supported version of the openPMD standard (read & write, compile-time)
