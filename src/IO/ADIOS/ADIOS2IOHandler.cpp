9e201df4e20f3cf41b75df55a19ae9a33f57cdae
guj <guj@users.noreply.github.com>
2021-01-21 12:35:08 -0800

    support null core instead of null (#906)
    * support null core instead of null
    
    * changed engine from null to nullcore
    
    * Add streaming test for nullcore engine
    
    * Update file endings list
    
    Co-authored-by: Franz Pöschel <franz.poeschel@gmail.com>

--- src/IO/ADIOS/ADIOS2IOHandler.cpp
+++ src/IO/ADIOS/ADIOS2IOHandler.cpp
@@ -210,7 +210,7 @@ ADIOS2IOHandlerImpl::fileSuffix() const
     static std::map< std::string, std::string > endings{
         { "sst", "" }, { "staging", "" }, { "bp4", ".bp" },
         { "bp3", ".bp" },  { "file", ".bp" },     { "hdf5", ".h5" },
-        { "null", ".null" }
+        { "nullcore", ".nullcore" }
     };
     auto it = endings.find( m_engineType );
     if( it != endings.end() )
@@ -1530,7 +1530,7 @@ namespace detail
     void BufferedActions::configure_IO(ADIOS2IOHandlerImpl& impl){
         ( void )impl;
         static std::set< std::string > streamingEngines = {
-            "sst", "insitumpi", "inline", "staging", "null"
+            "sst", "insitumpi", "inline", "staging", "nullcore"
         };
         static std::set< std::string > fileEngines = {
             "bp4", "bp3", "hdf5", "file"
