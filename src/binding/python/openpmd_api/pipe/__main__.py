0c425e72301ede700a4f5e17e10e1be361591999
Franz Pöschel <franz.poeschel@gmail.com>
2021-02-02 12:51:23 +0100

    Add openpmd_api.pipe module and executable
    This includes install scripts and tests in CMake

---
+++ src/binding/python/openpmd_api/pipe/__main__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This file is part of the openPMD-api.
+
+This module provides functions that are wrapped into sys.exit(...()) calls by
+the setuptools (setup.py) "entry_points" -> "console_scripts" generator.
+
+Copyright 2021 openPMD contributors
+Authors: Franz Poeschel
+License: LGPLv3+
+"""
+from . import main
+import sys
+
+if __name__ == "__main__":
+    main()
+    sys.exit()
