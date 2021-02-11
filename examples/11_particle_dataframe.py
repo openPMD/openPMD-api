83e7bd3ba0faa28c4c22b9c0cb046d8f57a552cc
Axel Huebl <axel.huebl@plasma.ninja>
2021-02-10 16:58:02 -0800

    CI: Pandas (Optional)

--- examples/11_particle_dataframe.py
+++ examples/11_particle_dataframe.py
@@ -8,20 +8,28 @@ License: LGPLv3+
 """
 import openpmd_api as io
 import numpy as np
-
+import sys
 try:
     import pandas as pd
-
-    s = io.Series("../samples/git-sample/data%T.h5", io.Access.read_only)
-    electrons = s.iterations[400].particles["electrons"]
-
-    # all particles
-    df = electrons.to_df()
-    print(df)
-
-    # only first 100 particles
-    df = electrons.to_df(np.s_[:100])
-    print(df)
-
 except ImportError:
     print("pandas NOT found. Install pandas to run this example.")
+    sys.exit()
+
+
+s = io.Series("../samples/git-sample/data%T.h5", io.Access.read_only)
+electrons = s.iterations[400].particles["electrons"]
+
+# all particles
+df = electrons.to_df()
+print(type(df) is pd.DataFrame)
+print(df)
+
+# only first 100 particles
+df = electrons.to_df(np.s_[:100])
+print(df)
+
+# get optimal chunks
+for chunk in electrons["position"]["x"].available_chunks():
+    stride = np.s_[chunk.offset[0]:chunk.extent[0]]
+    df = electrons.to_df()
+    print(df)
