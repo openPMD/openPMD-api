15a98c7605f353fae4fa7e8f2209de650e3f8f5a
Axel Huebl <axel.huebl@plasma.ninja>
2021-03-17 00:48:44 -0700

    ParticleSpecies: Read to dask.dataframe (#935)
    * ParticleSpecies: Read to dask.dataframe
    * put delayed into a list
    * Dask Example: Histogram Usage w/ Weight
    * Dask Chunks: Fallback to One
    
    Co-authored-by: Dmitry Ganyushin <ganyushin@gmail.com>

--- examples/11_particle_dataframe.py
+++ examples/11_particle_dataframe.py
@@ -3,7 +3,7 @@
 This file is part of the openPMD-api.
 
 Copyright 2021 openPMD contributors
-Authors: Axel Huebl
+Authors: Axel Huebl, Dmitry Ganyushin
 License: LGPLv3+
 """
 import openpmd_api as io
@@ -14,6 +14,13 @@ try:
 except ImportError:
     print("pandas NOT found. Install pandas to run this example.")
     sys.exit()
+found_dask = False
+try:
+    from dask.delayed import delayed
+    import dask.array as da
+    found_dask = True
+except ImportError:
+    print("dask NOT found. Install dask to run the 2nd example.")
 
 
 s = io.Series("../samples/git-sample/data%T.h5", io.Access.read_only)
@@ -28,8 +35,25 @@ print(df)
 df = electrons.to_df(np.s_[:100])
 print(df)
 
-# get optimal chunks
-for chunk in electrons["position"]["x"].available_chunks():
-    stride = np.s_[chunk.offset[0]:chunk.extent[0]]
-    df = electrons.to_df()
+
+# Dask example: process large datasets
+if found_dask:
+    df = electrons.to_dask()
     print(df)
+
+    # check chunking of a variable
+    print("chunking={}".format(df["momentum_z"].to_dask_array()))
+
+    # trigger a couple of compute actions
+    #   example1: average momentum in z
+    print("<momentum_z>={}".format(df["momentum_z"].mean().compute()))
+
+    #   example2: momentum histogram
+    h, bins = da.histogram(df["momentum_z"].to_dask_array(), bins=50,
+                           range=[-8.0e-23, 8.0e-23],
+                           weights=df["weighting"].to_dask_array())
+    print(h.compute())
+
+    #   example3: save all data data to parquet files
+    delayed_save = delayed(df.to_parquet("electrons.parquet"))
+    delayed_save.compute()
