4d057e179a1ad7e115a3f72ce81a43d692609141
Axel Huebl <axel.huebl@plasma.ninja>
2021-06-03 16:46:25 -0700

    add reference type, too

--- include/openPMD/WriteIterations.hpp
+++ include/openPMD/WriteIterations.hpp
@@ -55,6 +55,7 @@ public:
     using key_type = typename iterations_t::key_type;
     using mapped_type = typename iterations_t::mapped_type;
     using value_type = typename iterations_t::value_type;
+    using reference = typename iterations_t::reference;
 
 private:
     struct SharedResources
