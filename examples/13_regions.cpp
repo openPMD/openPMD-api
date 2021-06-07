11d512df12973ba554689fa7aea62f46024246c5
Erik Schnetter <schnetter@gmail.com>
2021-06-07 15:13:59 -0400

    Add example. Use C++17 for regions.

---
+++ examples/13_regions.cpp
@@ -0,0 +1,38 @@
+#include <openPMD/regions/Regions.hpp>
+
+#include <iostream>
+
+int main() {
+  using namespace openPMD::Regions;
+  using namespace std;
+
+  // Two points
+  Point<int, 3> x{1, 2, 3}, y{4, 5, 6};
+  auto z = x + y;
+  cout << "Points:\n"
+       << "x: " << x << "\n"
+       << "y: " << y << "\n";
+
+  // A box between these points
+  Box<int, 3> b(x, y);
+  auto bg = b.grown(1);
+  cout << "Boxes are spanned between points (inclusive lower, exclusive upper bound):\n"
+       << "b:  " << b << "\n"
+       << "bg: " << bg << "\n";
+
+  // Regions
+  Region<int, 3> r(b);
+  auto g = Region<int, 3>(bg) - r;
+  cout << "Regions are sets of non-overlapping boxes:\n"
+       << "r: " << r << "\n"
+       << "g: " << g << "\n";
+
+  {
+    NDPoint<int> p0(0);
+    NDPoint<int> p1(1);
+    NDPoint<int> p2(2);
+    NDPoint<int> p3(3);
+  }
+
+  return 0;
+}
