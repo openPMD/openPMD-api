686d14d9e6f67314dcfef0e3ae293d349d00296f
Erik Schnetter <schnetter@gmail.com>
2021-06-08 15:06:16 -0400

    Beautify output in example 13

--- examples/13_regions.cpp
+++ examples/13_regions.cpp
@@ -10,24 +10,24 @@ int main() {
   Point<int, 2> x{1, 2}, y{4, 5};
   auto z = x + y;
   cout << "Points:\n"
-       << "x: " << x << "\n"
-       << "y: " << y << "\n"
-       << "z: " << z << "\n";
+       << "  x: " << x << "\n"
+       << "  y: " << y << "\n"
+       << "  z: " << z << "\n";
 
   // A box between these points
   Box<int, 2> b(x, y);
   auto bg = b.grown(1);
   cout << "Boxes are spanned between points (inclusive lower, exclusive upper "
           "bound):\n"
-       << "b:  " << b << "\n"
-       << "bg: " << bg << "\n";
+       << "  b:  " << b << "\n"
+       << "  bg: " << bg << "\n";
 
   // Regions
   Region<int, 2> r(b);
   auto g = Region<int, 2>(bg) - r;
   cout << "Regions are sets of non-overlapping boxes:\n"
-       << "r: " << r << "\n"
-       << "g: " << g << "\n";
+       << "  r: " << r << "\n"
+       << "  g: " << g << "\n";
 
   {
     NDPoint<int> p0(0);
