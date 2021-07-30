1f6559bd7891fa1bbd0313c84137289dc8cc6757
Erik Schnetter <schnetter@gmail.com>
2021-07-30 09:59:18 -0400

    Julia: Rename SeriesImpl -> SeriesInterface

--- src/binding/julia/Series.cpp
+++ src/binding/julia/Series.cpp
@@ -4,62 +4,68 @@
 
 // Define supertype relationships
 namespace jlcxx {
-template <> struct SuperType<SeriesImpl> { typedef Attributable type; };
-template <> struct SuperType<Series> { typedef SeriesImpl type; };
+template <> struct SuperType<SeriesInterface> { typedef Attributable type; };
+template <> struct SuperType<Series> { typedef SeriesInterface type; };
 } // namespace jlcxx
 
 void define_julia_Series(jlcxx::Module &mod) {
 
-  // SeriesImpl
+  // SeriesInterface
   {
-    auto type = mod.add_type<SeriesImpl>(
-        "CXX_SeriesImpl", jlcxx::julia_base_type<Attributable>());
+    auto type = mod.add_type<SeriesInterface>(
+        "CXX_SeriesInterface", jlcxx::julia_base_type<Attributable>());
 
-    type.method("cxx_openPMD_version", &SeriesImpl::openPMD);
-    type.method("cxx_set_openPMD_version!", &SeriesImpl::setOpenPMD);
-    type.method("cxx_openPMD_extension", &SeriesImpl::openPMDextension);
-    type.method("cxx_set_openPMD_extension!", &SeriesImpl::setOpenPMDextension);
-    type.method("cxx_base_path", &SeriesImpl::basePath);
-    type.method("cxx_set_base_path!", &SeriesImpl::setBasePath);
-    type.method("cxx_meshes_path", &SeriesImpl::meshesPath);
-    type.method("cxx_set_meshes_path!", &SeriesImpl::setMeshesPath);
-    type.method("cxx_particles_path", &SeriesImpl::particlesPath);
-    type.method("cxx_set_particles_path!", &SeriesImpl::setParticlesPath);
-    type.method("cxx_author", &SeriesImpl::author);
-    type.method("cxx_set_author!", &SeriesImpl::setAuthor);
-    type.method("cxx_software", &SeriesImpl::software);
+    type.method("cxx_openPMD_version", &SeriesInterface::openPMD);
+    type.method("cxx_set_openPMD_version!", &SeriesInterface::setOpenPMD);
+    type.method("cxx_openPMD_extension", &SeriesInterface::openPMDextension);
+    type.method("cxx_set_openPMD_extension!",
+                &SeriesInterface::setOpenPMDextension);
+    type.method("cxx_base_path", &SeriesInterface::basePath);
+    type.method("cxx_set_base_path!", &SeriesInterface::setBasePath);
+    type.method("cxx_meshes_path", &SeriesInterface::meshesPath);
+    type.method("cxx_set_meshes_path!", &SeriesInterface::setMeshesPath);
+    type.method("cxx_particles_path", &SeriesInterface::particlesPath);
+    type.method("cxx_set_particles_path!", &SeriesInterface::setParticlesPath);
+    type.method("cxx_author", &SeriesInterface::author);
+    type.method("cxx_set_author!", &SeriesInterface::setAuthor);
+    type.method("cxx_software", &SeriesInterface::software);
+    type.method(
+        "cxx_set_software!",
+        static_cast<SeriesInterface &(SeriesInterface::*)(std::string const &,
+                                                          std::string const &)>(
+            &SeriesInterface::setSoftware));
     type.method("cxx_set_software!",
-                static_cast<SeriesImpl &(SeriesImpl::*)(std::string const &,
-                                                        std::string const &)>(
-                    &SeriesImpl::setSoftware));
-    type.method("cxx_set_software!",
-                (SeriesImpl & (SeriesImpl::*)(std::string const &))(
-                    &SeriesImpl::setSoftware));
-    type.method("cxx_software_version", &SeriesImpl::softwareVersion);
-    // type.method("set_software_version!", &SeriesImpl::setSoftwareVersion);
-    type.method("cxx_date", &SeriesImpl::date);
-    type.method("cxx_set_date!", &SeriesImpl::setDate);
-    type.method("cxx_software_dependencies", &SeriesImpl::softwareDependencies);
+                (SeriesInterface & (SeriesInterface::*)(std::string const &))(
+                    &SeriesInterface::setSoftware));
+    type.method("cxx_software_version", &SeriesInterface::softwareVersion);
+    // type.method("set_software_version!",
+    // &SeriesInterface::setSoftwareVersion);
+    type.method("cxx_date", &SeriesInterface::date);
+    type.method("cxx_set_date!", &SeriesInterface::setDate);
+    type.method("cxx_software_dependencies",
+                &SeriesInterface::softwareDependencies);
     type.method("cxx_set_software_dependencies!",
-                &SeriesImpl::setSoftwareDependencies);
-    type.method("cxx_machine", &SeriesImpl::machine);
-    type.method("cxx_set_machine!", &SeriesImpl::setMachine);
-    // TODO: type.method("iteration_encoding", &SeriesImpl::iterationEncoding);
+                &SeriesInterface::setSoftwareDependencies);
+    type.method("cxx_machine", &SeriesInterface::machine);
+    type.method("cxx_set_machine!", &SeriesInterface::setMachine);
+    // TODO: type.method("iteration_encoding",
+    // &SeriesInterface::iterationEncoding);
     // TODO: type.method("set_iteration_encoding!",
-    // &SeriesImpl::setIterationEncoding);
-    type.method("cxx_iteration_format", &SeriesImpl::iterationFormat);
-    type.method("cxx_set_iteration_format!", &SeriesImpl::setIterationFormat);
-    type.method("cxx_name", &SeriesImpl::name);
-    type.method("cxx_set_name!", &SeriesImpl::setName);
-    type.method("cxx_backend", &SeriesImpl::backend);
-    type.method("cxx_flush", &SeriesImpl::flush);
+    // &SeriesInterface::setIterationEncoding);
+    type.method("cxx_iteration_format", &SeriesInterface::iterationFormat);
+    type.method("cxx_set_iteration_format!",
+                &SeriesInterface::setIterationFormat);
+    type.method("cxx_name", &SeriesInterface::name);
+    type.method("cxx_set_name!", &SeriesInterface::setName);
+    type.method("cxx_backend", &SeriesInterface::backend);
+    type.method("cxx_flush", &SeriesInterface::flush);
   }
 
   // Series
 
   {
     auto type = mod.add_type<Series>("CXX_Series",
-                                     jlcxx::julia_base_type<SeriesImpl>());
+                                     jlcxx::julia_base_type<SeriesInterface>());
 
     type.constructor<>();
 #if openPMD_HAVE_MPI
