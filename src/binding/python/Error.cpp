e159ea014f0d84d97f475b79a3bdf56e3c4a3b2a
Franz Pöschel <franz.poeschel@gmail.com>
2021-08-09 10:41:42 +0200

    Python bindings for WrongAPIUsage

--- src/binding/python/Error.cpp
+++ src/binding/python/Error.cpp
@@ -10,6 +10,8 @@ void init_Error( py::module & m )
     auto & baseError = py::register_exception< Error >( m, "Error" );
     py::register_exception< error::OperationUnsupportedInBackend >(
         m, "ErrorOperationUnsupportedInBackend", baseError );
+    py::register_exception< error::WrongAPIUsage >(
+        m, "ErrorWrongAPIUsage", baseError );
 
 #ifndef NDEBUG
     m.def( "test_throw", []( std::string description ) {
