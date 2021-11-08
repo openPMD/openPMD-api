4905e1c69ac25e1932592f06e0db576a24da3b6c
Franz Pöschel <franz.poeschel@gmail.com>
2021-11-08 12:05:52 +0100

    Extend Python-based streaming test
    This makes the resulting dataset a bit more realistic.

--- examples/10_streaming_read.py
+++ examples/10_streaming_read.py
@@ -2,18 +2,43 @@
 import openpmd_api as io
 import sys
 
+# pass-through for ADIOS2 engine parameters
+# https://adios2.readthedocs.io/en/latest/engines/engines.html
+config = """
+{
+  "adios2": {
+    "engine": {
+      "parameters": {
+        "Threads": "4"
+      }
+    },
+    "dataset": {
+      "operators": [
+        {
+          "type": "bzip2"
+        }
+      ]
+    }
+  }
+}
+"""
+
 if __name__ == "__main__":
-    if 'adios2' not in io.variants or not io.variants['adios2']:
-        print('This example requires ADIOS2')
-        sys.exit(0)
-
-    series = io.Series("stream.sst", io.Access_Type.read_only)
-
+    # this block is for our CI, SST engine is not present on all systems
     backends = io.file_extensions
     if "sst" not in backends:
         print("SST engine not available in ADIOS2.")
         sys.exit(0)
 
+    series = io.Series("simData.sst", io.Access_Type.read_only, config)
+
+    # Read all available iterations and print electron position data.
+    # Use `series.read_iterations()` instead of `series.iterations`
+    # for streaming support (while still retaining file-reading support).
+    # Direct access to `series.iterations` is only necessary for random-access
+    # of iterations. By using `series.read_iterations()`, the openPMD-api will
+    # step through the iterations one by one, and going back to an iteration is
+    # not possible once it has been closed.
     for iteration in series.read_iterations():
         print("Current iteration {}".format(iteration.iteration_index))
         electronPositions = iteration.particles["e"]["position"]
@@ -26,8 +51,14 @@ if __name__ == "__main__":
             rc = electronPositions[dim]
             loadedChunks.append(rc.load_chunk([0], rc.shape))
             shapes.append(rc.shape)
+
+        # Closing the iteration loads all data and releases the current
+        # streaming step.
+        # If the iteration is not closed, it will be implicitly closed upon
+        # opening the next iteration.
         iteration.close()
 
+        # data is now available for printing
         for i in range(3):
             dim = dimensions[i]
             shape = shapes[i]
