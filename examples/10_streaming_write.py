8e92cc5f788b0697543b5b81693e7189db52bdd8
Franz Pöschel <franz.poeschel@gmail.com>
2021-12-02 22:32:22 +0100

    Add new Python-based linear/streaming API tests (#1141)
    * Run Python-based streaming test in CI
    
    We forgot that until now
    
    * Extend Python-based streaming test
    
    This makes the resulting dataset a bit more realistic.
    
    * Fix overlong line
    
    * Apply suggestions from code review
    
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>
    
    * Use Python JSON objects
    
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>

--- examples/10_streaming_write.py
+++ examples/10_streaming_write.py
@@ -1,33 +1,81 @@
 #!/usr/bin/env python
+import json
 import openpmd_api as io
 import numpy as np
 import sys
 
+# pass-through for ADIOS2 engine parameters
+# https://adios2.readthedocs.io/en/latest/engines/engines.html
+config = {'adios2': {'engine': {}, 'dataset': {}}}
+config['adios2']['engine'] = {'parameters': {'Threads': '4'}}
+config['adios2']['dataset'] = {'operators': [{'type': 'bzip2'}]}
+
 if __name__ == "__main__":
-    if 'adios2' not in io.variants or not io.variants['adios2']:
-        print('This example requires ADIOS2')
-        sys.exit(0)
-
-    series = io.Series("stream.sst", io.Access_Type.create)
-
+    # this block is for our CI, SST engine is not present on all systems
     backends = io.file_extensions
     if "sst" not in backends:
         print("SST engine not available in ADIOS2.")
         sys.exit(0)
 
-    datatype = np.dtype("double")
-    length = 10
-    global_extent = [10]
-    dataset = io.Dataset(datatype, global_extent)
+    # create a series and specify some global metadata
+    # change the file extension to .json, .h5 or .bp for regular file writing
+    series = io.Series("simData.sst", io.Access_Type.create,
+                       json.dumps(config))
+    series.set_author("Franz Poeschel <f.poeschel@hzdr.de>")
+    series.set_software("openPMD-api-python-examples")
 
-    iterations = series.write_iterations()
-    for i in range(100):
-        iteration = iterations[i]
+    # now, write a number of iterations (or: snapshots, time steps)
+    for i in range(10):
+        # Use `series.write_iterations()` instead of `series.iterations`
+        # for streaming support (while still retaining file-writing support).
+        # Direct access to `series.iterations` is only necessary for
+        # random-access of iterations. By using `series.write_iterations()`,
+        # the openPMD-api will adhere to streaming semantics while writing.
+        # In particular, this means that only one iteration can be written at a
+        # time and an iteration can no longer be modified after closing it.
+        iteration = series.write_iterations()[i]
+
+        #######################
+        # write electron data #
+        #######################
+
         electronPositions = iteration.particles["e"]["position"]
 
-        local_data = np.arange(i * length, (i + 1) * length, dtype=datatype)
+        # openPMD attribute
+        # (this one would also be set automatically for positions)
+        electronPositions.unit_dimension = {io.Unit_Dimension.L: 1.0}
+        # custom attribute
+        electronPositions.set_attribute("comment", "I'm a comment")
+
+        length = 10
+        local_data = np.arange(i * length, (i + 1) * length,
+                               dtype=np.dtype("double"))
         for dim in ["x", "y", "z"]:
             pos = electronPositions[dim]
-            pos.reset_dataset(dataset)
+            pos.reset_dataset(io.Dataset(local_data.dtype, [length]))
             pos[()] = local_data
+
+        # optionally: flush now to clear buffers
+        iteration.series_flush()  # this is a shortcut for `series.flush()`
+
+        ###############################
+        # write some temperature data #
+        ###############################
+
+        temperature = iteration.meshes["temperature"]
+        temperature.unit_dimension = {io.Unit_Dimension.theta: 1.0}
+        temperature.axis_labels = ["x", "y"]
+        temperature.grid_spacing = [1., 1.]
+        # temperature has no x,y,z components, so skip the last layer:
+        temperature_dataset = temperature[io.Mesh_Record_Component.SCALAR]
+        # let's say we are in a 3x3 mesh
+        temperature_dataset.reset_dataset(
+            io.Dataset(np.dtype("double"), [3, 3]))
+        # temperature is constant
+        temperature_dataset.make_constant(273.15)
+
+        # After closing the iteration, the readers can see the iteration.
+        # It can no longer be modified.
+        # If not closing an iteration explicitly, it will be implicitly closed
+        # upon creating the next iteration.
         iteration.close()
