1228708ab9e55c172d4eba9fc01f9ef49a585628
Erik Schnetter <schnetter@gmail.com>
2021-12-31 14:51:39 -0500

    Avoid unused-parameter warning

--- src/binding/julia/RecordComponent_store_chunk.cpp
+++ src/binding/julia/RecordComponent_store_chunk.cpp
@@ -3,7 +3,7 @@
 #include "defs.hpp"
 
 void define_julia_RecordComponent_store_chunk(
-    jlcxx::Module &mod, jlcxx::TypeWrapper<RecordComponent> &type) {
+    jlcxx::Module & /*mod*/, jlcxx::TypeWrapper<RecordComponent> &type) {
 #define USE_TYPE(NAME, ENUM, TYPE)                                             \
   type.method("cxx_store_chunk_" NAME,                                         \
               static_cast<void (RecordComponent::*)(std::shared_ptr<TYPE>,     \
