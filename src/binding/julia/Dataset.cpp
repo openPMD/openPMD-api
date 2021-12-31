218b67e18d0c67ade7872915cbe5260341f0531f
Erik Schnetter <schnetter@gmail.com>
2021-12-30 22:16:54 -0500

    Update Julia interface

--- src/binding/julia/Dataset.cpp
+++ src/binding/julia/Dataset.cpp
@@ -10,14 +10,17 @@ void define_julia_Dataset(jlcxx::Module &mod) {
   type.constructor<Extent>();
 
   type.method("cxx_extend!", &Dataset::extend);
-  type.method("cxx_set_chunk_size!", &Dataset::setChunkSize);
-  type.method("set_compression!", &Dataset::setCompression);
-  type.method("set_custom_transform!", &Dataset::setCustomTransform);
+  // TODO type.method("cxx_set_chunk_size!", &Dataset::setChunkSize);
+  // TODO type.method("set_compression!", &Dataset::setCompression);
+  // TODO type.method("set_custom_transform!", &Dataset::setCustomTransform);
   type.method("cxx_extent", [](const Dataset &d) { return d.extent; });
   type.method("cxx_dtype", [](const Dataset &d) { return d.dtype; });
   type.method("cxx_rank", [](const Dataset &d) { return d.rank; });
-  type.method("cxx_chunk_size", [](const Dataset &d) { return d.chunkSize; });
-  type.method("compression", [](const Dataset &d) { return d.compression; });
-  type.method("transform", [](const Dataset &d) { return d.transform; });
+  // TODO type.method("cxx_chunk_size", [](const Dataset &d) { return
+  // d.chunkSize; });
+  // TODO type.method("compression", [](const Dataset &d) { return
+  // d.compression; });
+  // TODO type.method("transform", [](const Dataset &d) { return d.transform;
+  // });
   type.method("options", [](const Dataset &d) { return d.options; });
 }
--- src/binding/julia/Series.cpp
+++ src/binding/julia/Series.cpp
@@ -12,92 +12,78 @@ template <> struct IsMirroredType<MPI_Comm> : std::false_type {};
 
 // Define supertype relationships
 namespace jlcxx {
-template <> struct SuperType<SeriesInterface> { typedef Attributable type; };
-template <> struct SuperType<Series> { typedef SeriesInterface type; };
+template <> struct SuperType<Series> { typedef Attributable type; };
 } // namespace jlcxx
 
 void define_julia_Series(jlcxx::Module &mod) {
 
-  // SeriesInterface
-  {
-    auto type = mod.add_type<SeriesInterface>(
-        "CXX_SeriesInterface", jlcxx::julia_base_type<Attributable>());
-
-    type.method("cxx_openPMD_version", &SeriesInterface::openPMD);
-    type.method("cxx_set_openPMD_version!", &SeriesInterface::setOpenPMD);
-    type.method("cxx_openPMD_extension", &SeriesInterface::openPMDextension);
-    type.method("cxx_set_openPMD_extension!",
-                &SeriesInterface::setOpenPMDextension);
-    type.method("cxx_base_path", &SeriesInterface::basePath);
-    type.method("cxx_set_base_path!", &SeriesInterface::setBasePath);
-    type.method("cxx_meshes_path", &SeriesInterface::meshesPath);
-    type.method("cxx_set_meshes_path!", &SeriesInterface::setMeshesPath);
-    type.method("cxx_particles_path", &SeriesInterface::particlesPath);
-    type.method("cxx_set_particles_path!", &SeriesInterface::setParticlesPath);
-    type.method("cxx_author", &SeriesInterface::author);
-    type.method("cxx_set_author!", &SeriesInterface::setAuthor);
-    type.method("cxx_software", &SeriesInterface::software);
-    type.method(
-        "cxx_set_software!",
-        static_cast<SeriesInterface &(SeriesInterface::*)(std::string const &,
-                                                          std::string const &)>(
-            &SeriesInterface::setSoftware));
-    type.method("cxx_set_software!",
-                (SeriesInterface & (SeriesInterface::*)(std::string const &))(
-                    &SeriesInterface::setSoftware));
-    type.method("cxx_software_version", &SeriesInterface::softwareVersion);
-    // type.method("set_software_version!",
-    // &SeriesInterface::setSoftwareVersion);
-    type.method("cxx_date", &SeriesInterface::date);
-    type.method("cxx_set_date!", &SeriesInterface::setDate);
-    type.method("cxx_software_dependencies",
-                &SeriesInterface::softwareDependencies);
-    type.method("cxx_set_software_dependencies!",
-                &SeriesInterface::setSoftwareDependencies);
-    type.method("cxx_machine", &SeriesInterface::machine);
-    type.method("cxx_set_machine!", &SeriesInterface::setMachine);
-    // TODO: type.method("iteration_encoding",
-    // &SeriesInterface::iterationEncoding);
-    // TODO: type.method("set_iteration_encoding!",
-    // &SeriesInterface::setIterationEncoding);
-    type.method("cxx_iteration_format", &SeriesInterface::iterationFormat);
-    type.method("cxx_set_iteration_format!",
-                &SeriesInterface::setIterationFormat);
-    type.method("cxx_name", &SeriesInterface::name);
-    type.method("cxx_set_name!", &SeriesInterface::setName);
-    type.method("cxx_backend", &SeriesInterface::backend);
-    type.method("cxx_flush", &SeriesInterface::flush);
-  }
-
   // Series
 
-  {
-    auto type = mod.add_type<Series>("CXX_Series",
-                                     jlcxx::julia_base_type<SeriesInterface>());
+  auto type = mod.add_type<Series>("CXX_Series",
+                                   jlcxx::julia_base_type<Attributable>());
 
-    type.constructor<>();
+  type.constructor<>();
 #if openPMD_HAVE_MPI
-    type.method("cxx_Series", [](const std::string &filepath, Access at,
-                                 sized_uint_t<sizeof(MPI_Comm)> comm,
-                                 const std::string &options) {
-      return Series(filepath, at, *(const MPI_Comm *)&comm, options);
-    });
-    type.method("cxx_Series", [](const std::string &filepath, Access at,
-                                 sized_uint_t<sizeof(MPI_Comm)> comm) {
-      return Series(filepath, at, *(const MPI_Comm *)&comm);
-    });
+  type.method("cxx_Series", [](const std::string &filepath, Access at,
+                               sized_uint_t<sizeof(MPI_Comm)> comm,
+                               const std::string &options) {
+    return Series(filepath, at, *(const MPI_Comm *)&comm, options);
+  });
+  type.method("cxx_Series", [](const std::string &filepath, Access at,
+                               sized_uint_t<sizeof(MPI_Comm)> comm) {
+    return Series(filepath, at, *(const MPI_Comm *)&comm);
+  });
 #endif
-    type.constructor<const std::string &, Access, const std::string &>();
-    type.constructor<const std::string &, Access>();
+  type.constructor<const std::string &, Access, const std::string &>();
+  type.constructor<const std::string &, Access>();
 
-    type.method("cxx_isvalid",
-                [](const Series &series) { return bool(series); });
-    type.method("cxx_close", &Series::close);
-    type.method("cxx_iterations",
-                [](Series &series) -> Container<Iteration, uint64_t> & {
-                  return series.iterations;
-                });
-    // TODO type.method("read_iterations", &Series::readIterations);
-    type.method("cxx_write_iterations", &Series::writeIterations);
-  }
+  type.method("cxx_isvalid", [](const Series &series) { return bool(series); });
+
+  type.method("cxx_openPMD_version", &Series::openPMD);
+  type.method("cxx_set_openPMD_version!", &Series::setOpenPMD);
+  type.method("cxx_openPMD_extension", &Series::openPMDextension);
+  type.method("cxx_set_openPMD_extension!", &Series::setOpenPMDextension);
+  type.method("cxx_base_path", &Series::basePath);
+  type.method("cxx_set_base_path!", &Series::setBasePath);
+  type.method("cxx_meshes_path", &Series::meshesPath);
+  type.method("cxx_set_meshes_path!", &Series::setMeshesPath);
+  type.method("cxx_particles_path", &Series::particlesPath);
+  type.method("cxx_set_particles_path!", &Series::setParticlesPath);
+  type.method("cxx_author", &Series::author);
+  type.method("cxx_set_author!", &Series::setAuthor);
+  type.method("cxx_software", &Series::software);
+  type.method("cxx_set_software!",
+              static_cast<Series &(Series::*)(std::string const &,
+                                              std::string const &)>(
+                  &Series::setSoftware));
+  type.method("cxx_set_software!", (Series & (Series::*)(std::string const &))(
+                                       &Series::setSoftware));
+  type.method("cxx_software_version", &Series::softwareVersion);
+  // type.method("set_software_version!",
+  // &Series::setSoftwareVersion);
+  type.method("cxx_date", &Series::date);
+  type.method("cxx_set_date!", &Series::setDate);
+  type.method("cxx_software_dependencies", &Series::softwareDependencies);
+  type.method("cxx_set_software_dependencies!",
+              &Series::setSoftwareDependencies);
+  type.method("cxx_machine", &Series::machine);
+  type.method("cxx_set_machine!", &Series::setMachine);
+  // TODO: type.method("iteration_encoding",
+  // &Series::iterationEncoding);
+  // TODO: type.method("set_iteration_encoding!",
+  // &Series::setIterationEncoding);
+  type.method("cxx_iteration_format", &Series::iterationFormat);
+  type.method("cxx_set_iteration_format!", &Series::setIterationFormat);
+  type.method("cxx_name", &Series::name);
+  type.method("cxx_set_name!", &Series::setName);
+  type.method("cxx_backend", &Series::backend);
+  type.method("cxx_flush", &Series::flush);
+
+  // TODO type.method("cxx_close", &Series::close);
+  type.method("cxx_iterations",
+              [](Series &series) -> Container<Iteration, uint64_t> & {
+                return series.iterations;
+              });
+  // TODO type.method("read_iterations", &Series::readIterations);
+  type.method("cxx_write_iterations", &Series::writeIterations);
 }
