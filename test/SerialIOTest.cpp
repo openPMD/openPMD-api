15daaceb053f37ddd82fc2887f90ec955b7ef0aa
Franz Pöschel <franz.poeschel@gmail.com>
2022-01-07 15:12:00 +0100

    Use a test that works independent from CI config

--- test/SerialIOTest.cpp
+++ test/SerialIOTest.cpp
@@ -3347,21 +3347,26 @@ TEST_CASE( "serial_adios1_json_config", "[serial][adios1]" )
   "backend": "adios1",
   "adios1": {
     "dataset": {
-      "transform": "blosc:compressor=zlib,shuffle=bit,lvl=1;nometa"
-    }
-  }
-})END";
-    std::string localConfig = R"END(
-{
-  "adios1": {
-    "dataset": {
-      "transform": "blosc:compressor=zlib,shuffle=bit,lvl=5;nometa"
+      "transform": "deliberately use a wrong configuration..."
     }
   }
 })END";
 
-    try
-    {
+    std::string globalConfigWithoutCompression = R"END(
+{
+  "backend": "adios1"
+})END";
+
+    std::string localConfig = R"END(
+{
+  "adios1": {
+    "dataset": {
+      "transform": "...so we can check for the resulting errors"
+    }
+  }
+})END";
+
+    auto test1 = [ & ]() {
         Series write(
             "../samples/adios1_dataset_transform.bp",
             Access::CREATE,
@@ -3370,38 +3375,63 @@ TEST_CASE( "serial_adios1_json_config", "[serial][adios1]" )
 
         auto defaultConfiguredMesh =
             meshes[ "defaultConfigured" ][ RecordComponent::SCALAR ];
-        auto overridenTransformMesh =
-            meshes[ "overridenConfig" ][ RecordComponent::SCALAR ];
 
         Dataset ds{ Datatype::INT, { 10 } };
 
         defaultConfiguredMesh.resetDataset( ds );
 
+        std::vector< int > data( 10, 2345 );
+        defaultConfiguredMesh.storeChunk( data, { 0 }, { 10 } );
+
+        write.flush();
+    };
+    REQUIRE_THROWS_WITH(
+        test1(),
+        Catch::Equals( "[ADIOS1] Internal error: Failed to set ADIOS transform "
+                       "during Dataset creation" ) );
+
+    auto test2 = [ & ]() {
+        Series write(
+            "../samples/adios1_dataset_transform.bp",
+            Access::CREATE,
+            globalConfig );
+        auto meshes = write.writeIterations()[ 0 ].meshes;
+        auto overridenTransformMesh =
+            meshes[ "overridenConfig" ][ RecordComponent::SCALAR ];
+
+        Dataset ds{ Datatype::INT, { 10 } };
         ds.options = localConfig;
         overridenTransformMesh.resetDataset( ds );
 
         std::vector< int > data( 10, 2345 );
-
-        defaultConfiguredMesh.storeChunk( data, { 0 }, { 10 } );
         overridenTransformMesh.storeChunk( data, { 0 }, { 10 } );
 
         write.flush();
-    }
-    catch( std::runtime_error const & e )
-    {
-        if( std::string( e.what() ) ==
-            "[ADIOS1] Internal error: Failed to set ADIOS transform during "
-            "Dataset creation" )
-        {
-            std::cerr
-                << "Skipping serial_adios1_json_config test since the ADIOS1 "
-                   "installation does not support blosc compression.";
-        }
-        else
-        {
-            throw;
-        }
-    }
+    };
+    REQUIRE_THROWS_WITH(
+        test2(),
+        Catch::Equals( "[ADIOS1] Internal error: Failed to set ADIOS transform "
+                       "during Dataset creation" ) );
+
+    auto test3 = [ & ]() {
+        // use no dataset transform at all
+        Series write(
+            "../samples/adios1_dataset_transform.bp",
+            Access::CREATE,
+            globalConfigWithoutCompression );
+        auto meshes = write.writeIterations()[ 0 ].meshes;
+        auto defaultConfiguredMesh =
+            meshes[ "defaultConfigured" ][ RecordComponent::SCALAR ];
+
+        Dataset ds{ Datatype::INT, { 10 } };
+        defaultConfiguredMesh.resetDataset( ds );
+
+        std::vector< int > data( 10, 2345 );
+        defaultConfiguredMesh.storeChunk( data, { 0 }, { 10 } );
+
+        write.flush();
+    };
+    test3(); // should run without exception
 }
 #endif
 
