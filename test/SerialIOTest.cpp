39ed7bedbcd3b26d4ac35f604de008792b4573cc
Franz Pöschel <franz.poeschel@gmail.com>
2022-07-15 14:53:23 +0200

    Add failing test

--- test/SerialIOTest.cpp
+++ test/SerialIOTest.cpp
@@ -16,6 +16,7 @@
 
 #include <algorithm>
 #include <array>
+#include <chrono>
 #include <cmath>
 #include <complex>
 #include <fstream>
@@ -27,6 +28,7 @@
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <thread>
 #include <tuple>
 #include <vector>
 
@@ -6372,6 +6374,13 @@ void append_mode(
         write.flush();
     }
     {
+        using namespace std::chrono_literals;
+        /*
+         * Put a little sleep here to trigger writing of a different /date
+         * attribute. ADIOS2 v2.7 does not like that so this test ensures that
+         * we deal with it.
+         */
+        std::this_thread::sleep_for(1s);
         Series write(filename, Access::APPEND, jsonConfig);
         if (variableBased)
         {
@@ -6540,8 +6549,8 @@ void append_mode_filebased(std::string const &extension)
     }
     {
         Series write(
-            "../samples/append/append_%T." + extension,
-            Access::CREATE,
+            "../samples/append/append_%06T." + extension,
+            Access::APPEND,
             jsonConfig);
         writeSomeIterations(
             write.writeIterations(), std::vector<uint64_t>{0, 1});
