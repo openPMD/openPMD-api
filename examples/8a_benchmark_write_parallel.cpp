240ac5b97d655dd28aa9c198fe8be1db1f5f65ac
guj <jgu@lbl.gov>
2023-01-13 11:38:20 -0800

    fixed a bug in 1D test cases

--- examples/8a_benchmark_write_parallel.cpp
+++ examples/8a_benchmark_write_parallel.cpp
@@ -701,6 +701,7 @@ void AbstractPattern::run()
                 store(series, step);
             }
         }
+	return;
     }
 
     { // group/var based
@@ -1004,6 +1005,7 @@ bool OneDimPattern::setLayOut(int step)
         return true;
 
     auto numPartition = m_Input.GetSeg();
+    /*
     if (unitCount < numPartition)
         numPartition = unitCount;
 
@@ -1022,6 +1024,22 @@ bool OneDimPattern::setLayOut(int step)
             Extent count = {res * m_MinBlock[0]};
             m_InRankMeshLayout.emplace_back(offset, count);
         }
+    }
+    */
+    if ( 1 == numPartition )
+    {
+      Offset offset = { unitOffset * m_MinBlock[0] };
+      Extent count  = { unitCount * m_MinBlock[0]  };
+      m_InRankMeshLayout.emplace_back(offset, count);
+    }
+    else
+    {
+      Extent count  = { m_MinBlock[0]  };
+      for (unsigned long i=0; i<unitCount; i++)
+      {
+         Offset offset = { (unitOffset+i) * m_MinBlock[0]  };
+         m_InRankMeshLayout.emplace_back(offset, count);
+      }
     }
 
     return true;
