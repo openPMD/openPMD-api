896e95d3e90c93387f6d76742ac9e8634711e1a4
Franz Pöschel <franz.poeschel@gmail.com>
2023-03-02 13:46:10 +0100

    Python bindings: Chunk distribution algorithms

--- src/binding/python/ChunkInfo.cpp
+++ src/binding/python/ChunkInfo.cpp
@@ -19,12 +19,14 @@
  * If not, see <http://www.gnu.org/licenses/>.
  */
 #include "openPMD/ChunkInfo.hpp"
+#include "openPMD/benchmark/mpi/OneDimensionalBlockSlicer.hpp"
 #include "openPMD/binding/python/Mpi.hpp"
 
 #include "openPMD/binding/python/Common.hpp"
 
 #include <exception>
 #include <string>
+#include <utility> // std::move
 
 void init_Chunk(py::module &m)
 {
@@ -100,4 +102,91 @@ void init_Chunk(py::module &m)
                 return host_info::byMethod(self);
             })
         .def("available", &host_info::methodAvailable);
+
+
+    using namespace chunk_assignment;
+
+    (void)py::class_<PartialStrategy>(m, "PartialStrategy");
+
+    py::class_<PartialStrategy>(m, "PartialStrategy")
+        .def(
+            "assign",
+            py::overload_cast<ChunkTable, RankMeta const &, RankMeta const &>(
+                &PartialStrategy::assign),
+            py::arg("chunk_table"),
+            py::arg("rank_meta_in") = RankMeta(),
+            py::arg("rank_meta_out") = RankMeta())
+        .def(
+            "assign",
+            py::overload_cast<
+                PartialAssignment,
+                RankMeta const &,
+                RankMeta const &>(&PartialStrategy::assign),
+            py::arg("partial_assignment"),
+            py::arg("rank_meta_in") = RankMeta(),
+            py::arg("rank_meta_out") = RankMeta());
+
+    py::class_<Strategy>(m, "Strategy")
+        .def(
+            "assign",
+            py::overload_cast<ChunkTable, RankMeta const &, RankMeta const &>(
+                &Strategy::assign),
+            py::arg("chunk_table"),
+            py::arg("rank_meta_in") = RankMeta(),
+            py::arg("rank_meta_out") = RankMeta())
+        .def(
+            "assign",
+            py::overload_cast<
+                PartialAssignment,
+                RankMeta const &,
+                RankMeta const &>(&Strategy::assign),
+            py::arg("partial_assignment"),
+            py::arg("rank_meta_in") = RankMeta(),
+            py::arg("rank_meta_out") = RankMeta());
+
+    py::class_<FromPartialStrategy, Strategy>(m, "FromPartialStrategy")
+        .def(py::init([](PartialStrategy const &firstPass,
+                         Strategy const &secondPass) {
+            return FromPartialStrategy(firstPass.clone(), secondPass.clone());
+        }));
+
+    py::class_<RoundRobin, Strategy>(m, "RoundRobin").def(py::init<>());
+
+    py::class_<ByHostname, PartialStrategy>(m, "ByHostname")
+        .def(
+            py::init([](Strategy const &withinNode) {
+                return ByHostname(withinNode.clone());
+            }),
+            py::arg("strategy_within_node"));
+
+    (void)py::class_<BlockSlicer>(m, "BlockSlicer");
+
+    py::class_<OneDimensionalBlockSlicer, BlockSlicer>(
+        m, "OneDimensionalBlockSlicer")
+        .def(py::init<>())
+        .def(py::init<Extent::value_type>(), py::arg("dim"));
+
+    py::class_<ByCuboidSlice, Strategy>(m, "ByCuboidSlice")
+        .def(
+            py::init([](BlockSlicer const &blockSlicer,
+                        Extent totalExtent,
+                        unsigned int mpi_rank,
+                        unsigned int mpi_size) {
+                return ByCuboidSlice(
+                    blockSlicer.clone(),
+                    std::move(totalExtent),
+                    mpi_rank,
+                    mpi_size);
+            }),
+            py::arg("block_slicer"),
+            py::arg("total_extent"),
+            py::arg("mpi_rank"),
+            py::arg("mpi_size"));
+
+    py::class_<BinPacking, Strategy>(m, "BinPacking")
+        .def(py::init<>())
+        .def(py::init<size_t>(), py::arg("split_along_dimension"));
+
+    py::class_<FailingStrategy, Strategy>(m, "FailingStrategy")
+        .def(py::init<>());
 }
