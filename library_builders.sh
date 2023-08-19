fa1a0588c30f0c8de81cc5b6de4516106df06576
Axel Huebl <axel.huebl@plasma.ninja>
2023-08-18 20:28:24 -0600

    [Patch] ADIOS2: Fix ChunkV Issue & Static Builds

--- library_builders.sh
+++ library_builders.sh
@@ -103,10 +103,10 @@ function build_adios1 {
 function build_adios2 {
     if [ -e adios2-stamp ]; then return; fi
 
-    #curl -sLo adios2-2.9.0.tar.gz \
-    #    https://github.com/ornladios/ADIOS2/archive/v2.9.0.tar.gz
-    curl -sLo adios2-fix-blosc2-findpackage.tar.gz \
-        https://github.com/ax3l/ADIOS2/archive/refs/heads/fix-blosc2-findpackage.tar.gz
+    #curl -sLo adios2-2.9.1.tar.gz \
+    #    https://github.com/ornladios/ADIOS2/archive/v2.9.1.tar.gz
+    curl -sLo adios2-2.9.1.tar.gz \
+        https://github.com/ax3l/ADIOS2/archive/refs/heads/release-2.9.1-bp-wheels.tar.gz
     file adios2*.tar.gz
     tar -xzf adios2*.tar.gz
     rm adios2*.tar.gz
