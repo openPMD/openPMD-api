684173dae68f2f053cb1374375a9397fb7e69f2f
Axel Huebl <axel.huebl@plasma.ninja>
2023-08-19 23:46:20 -0700

    Windows: Upgrade CMake
    Only CMake 3.24+ supports static zlib.
    https://cmake.org/cmake/help/latest/module/FindZLIB.html#hints

--- library_builders.bat
+++ library_builders.bat
@@ -11,6 +11,7 @@ goto:main
 
 :install_buildessentials
   python -m pip install --upgrade pip setuptools wheel
+  python -m pip install --upgrade cmake
   python -m pip install --upgrade "patch==1.*"
 exit /b 0
 
