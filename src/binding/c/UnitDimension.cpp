a9d7baabf611e3277dfac3dbc0e465d720f6cb61
Erik Schnetter <schnetter@gmail.com>
2023-10-15 13:04:36 -0400

    Allow C99 code in C++

--- src/binding/c/UnitDimension.cpp
+++ src/binding/c/UnitDimension.cpp
@@ -1,3 +1,4 @@
-#include <openPMD/binding/c/UnitDimension.h>
+// Include `<UnitDimension.hpp>` with its identifier `I` before `<complex.h>`
+#include <openPMD/UnitDimension.hpp>
 
-// #include <openPMD/UnitDimension.hpp>
+#include <openPMD/binding/c/UnitDimension.h>
