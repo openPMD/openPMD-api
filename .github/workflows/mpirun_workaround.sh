f7976f11e30b594b4c2d552ab4f52af3eea33d24
Franz Pöschel <franz.poeschel@gmail.com>
2023-12-05 15:18:56 +0100

    Use mpirun wrapper to circumvent parser bug in mpirun
    The bug sounds similar to this one
    https://github.com/open-mpi/ompi/issues/6372, though that is supposedly
    fixed in Open MPI 5..
    
    This creates a tmp script to call the launched application instead of
    calling it directly on the command line. This way, mpirun does not see
    the command line arguments and cannot try to wrongly parse them.

---
+++ .github/workflows/mpirun_workaround.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# mpiexec currently seems to have a bug where it tries to parse parameters
+# of the launched application when they start with a dash, e.g.
+# `mpiexec python ./openpmd-pipe --infile in.bp --outfile out.bp`
+# leads to:
+# >  An unrecognized option was included on the mpiexec command line:
+# >
+# >    Option: --infile
+# >
+# >  Please use the "mpiexec --help" command to obtain a list of all
+# >  supported options.
+#
+# This script provides a workaround by putting the called sub-command into
+# a script in a temporary file.
+
+mpirun_args=()
+
+script_file="$(mktemp)"
+
+cleanup() {
+    rm "$script_file"
+}
+trap cleanup EXIT
+
+while true; do
+    case "$1" in
+        -c | -np | --np | -n | --n )
+            mpirun_args+=("$1" "$2")
+            shift
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+echo -e '#!/usr/bin/env bash\n' > "$script_file"
+for item in "$@"; do
+    echo -n "'$item' " >> "$script_file"
+done
+
+chmod +x "$script_file"
+
+mpirun "${mpirun_args[@]}" "$script_file"
