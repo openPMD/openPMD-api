e946cc2bde60c8d559160d800739321a09981f41
Franz Pöschel <franz.poeschel@gmail.com>
2024-01-26 13:39:41 +0100

    Make AbstractSeriesIterator non-virtual

--- src/Snapshots.cpp
+++ src/Snapshots.cpp
@@ -1,9 +1,11 @@
 #include "openPMD/Snapshots.hpp"
+#include <stdexcept>
 
 namespace openPMD
 {
 OpaqueSeriesIterator Snapshots::begin()
 {
     // return OpaqueSeriesIterator();
+    throw std::runtime_error("unimplemented");
 }
 } // namespace openPMD
