dd06287f69e8857dfa6ecd88234d71787804c808
Franz Pöschel <franz.poeschel@gmail.com>
2024-03-11 15:12:17 +0100

    Add particle patches to streaming example, using local_value

--- examples/10_streaming_read.cpp
+++ examples/10_streaming_read.cpp
@@ -19,6 +19,7 @@
  * If not, see <http://www.gnu.org/licenses/>.
  */
 #include <openPMD/openPMD.hpp>
+#include <openPMD/auxiliary/StringManip.hpp>
 
 #include <algorithm>
 #include <array>
@@ -73,6 +74,24 @@ int main()
             loadedChunks[i] = rc.loadChunkVariant(
                 Offset(rc.getDimensionality(), 0), rc.getExtent());
             extents[i] = rc.getExtent();
+        }
+
+        auto e_patches = iteration.particles["e"].particlePatches;
+        for (auto key :
+             {"numParticles", "numParticlesOffset", "offset", "extent"})
+        {
+            for (auto &rc : e_patches[key])
+            {
+                std::cout << "Chunks for '" << rc.second.myPath().openPMDPath()
+                          << "':";
+                for (auto const &chunk : rc.second.availableChunks())
+                {
+                    std::cout << "\n\tRank " << chunk.sourceID << "\t"
+                              << auxiliary::format_vec(chunk.offset) << "\t– "
+                              << auxiliary::format_vec(chunk.extent);
+                }
+                std::cout << std::endl;
+            }
         }
 
         // The iteration can be closed in order to help free up resources.
