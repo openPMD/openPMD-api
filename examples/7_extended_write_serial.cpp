aecdd8848b8170e2c745c243cf88f4fc75991693
Franz Pöschel <franz.poeschel@gmail.com>
2024-07-03 11:40:46 +0200

    Full support for the set_filter API

--- examples/7_extended_write_serial.cpp
+++ examples/7_extended_write_serial.cpp
@@ -120,6 +120,15 @@ int main()
         }
       ]
     }
+  },
+  "hdf5": {
+    "dataset": {
+      "chunks": "auto",
+      "permanent_filters": {
+        "id": "fletcher32",
+        "flags": "optional"
+      }
+    }
   }
 })END";
         d.options = datasetConfig;
@@ -154,7 +163,7 @@ int main()
 
                 hdf5.dataset.permanent_filters = [
                     {type = "zlib", aggression = 5},
-                    {id = "shuffle"}
+                    {id = "shuffle", "flags" = "MANDATORY"}
                 ]
             )");
         electrons.particlePatches["numParticles"].resetDataset(dset);
