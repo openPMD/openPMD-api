bcb1e17100db00eefddd7a2780e1ba78504d0da4
Axel Huebl <axel.huebl@plasma.ninja>
2024-10-08 10:44:26 -0700

    Fix: CMake Internal Control
    The `openPMD_USE_INTERNAL_*` options are for general internal/external
    control. They were accidentally overwritten by new tar flags
    and sometimes incomplete.
    
    Signed-off-by: Axel Huebl <axel.huebl@plasma.ninja>

--- cmake/dependencies/catch.cmake
+++ cmake/dependencies/catch.cmake
@@ -1,49 +1,49 @@
 function(find_catch2)
     if(TARGET Catch2::Catch2)
         message(STATUS "Catch2::Catch2 target already imported")
-    elseif(openPMD_catch_src)
-        message(STATUS "Compiling local Catch2 ...")
-        message(STATUS "Catch2 source path: ${openPMD_catch_src}")
-        if(NOT IS_DIRECTORY ${openPMD_catch_src})
-            message(FATAL_ERROR "Specified directory openPMD_catch_src='${openPMD_catch_src}' does not exist!")
-        endif()
-    elseif(openPMD_catch_tar)
-        message(STATUS "Downloading Catch2 ...")
-        message(STATUS "Catch2 source: ${openPMD_catch_tar}")
     elseif(openPMD_USE_INTERNAL_CATCH)
-        message(STATUS "Downloading Catch2 ...")
-        message(STATUS "Catch2 repository: ${openPMD_catch_repo} (${openPMD_catch_branch})")
+        if(openPMD_catch_src)
+            message(STATUS "Compiling local Catch2 ...")
+            message(STATUS "Catch2 source path: ${openPMD_catch_src}")
+            if(NOT IS_DIRECTORY ${openPMD_catch_src})
+                message(FATAL_ERROR "Specified directory openPMD_catch_src='${openPMD_catch_src}' does not exist!")
+            endif()
+        elseif(openPMD_catch_tar)
+            message(STATUS "Downloading Catch2 ...")
+            message(STATUS "Catch2 source: ${openPMD_catch_tar}")
+        elseif(openPMD_catch_branch)
+            message(STATUS "Downloading Catch2 ...")
+            message(STATUS "Catch2 repository: ${openPMD_catch_repo} (${openPMD_catch_branch})")
+        endif()
     endif()
     if(TARGET Catch2::Catch2)
         # nothing to do, target already exists in the superbuild
-    elseif(openPMD_USE_INTERNAL_CATCH OR openPMD_catch_src OR openPMD_catch_tar)
-        if(openPMD_catch_src)
-            add_subdirectory(${openPMD_catch_src} _deps/localCatch2-build/)
+    elseif(openPMD_USE_INTERNAL_CATCH AND openPMD_catch_src)
+        add_subdirectory(${openPMD_catch_src} _deps/localCatch2-build/)
+    elseif(openPMD_USE_INTERNAL_CATCH AND (openPMD_catch_tar OR openPMD_catch_branch))
+        include(FetchContent)
+        if(openPMD_catch_tar)
+            FetchContent_Declare(fetchedCatch2
+                URL             ${openPMD_catch_tar}
+                URL_HASH        ${openPMD_catch_tar_hash}
+                BUILD_IN_SOURCE OFF
+            )
         else()
-            include(FetchContent)
-            if(openPMD_catch_tar)
-                FetchContent_Declare(fetchedCatch2
-                    URL             ${openPMD_catch_tar}
-                    URL_HASH        ${openPMD_catch_tar_hash}
-                    BUILD_IN_SOURCE OFF
-                )
-            else()
-                FetchContent_Declare(fetchedCatch2
-                    GIT_REPOSITORY ${openPMD_catch_repo}
-                    GIT_TAG        ${openPMD_catch_branch}
-                    BUILD_IN_SOURCE OFF
-                )
-            endif()
-            FetchContent_MakeAvailable(fetchedCatch2)
-
-            # advanced fetch options
-            mark_as_advanced(FETCHCONTENT_BASE_DIR)
-            mark_as_advanced(FETCHCONTENT_FULLY_DISCONNECTED)
-            mark_as_advanced(FETCHCONTENT_QUIET)
-            #mark_as_advanced(FETCHCONTENT_SOURCE_DIR_FETCHEDCatch2)
-            mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED)
-            #mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED_FETCHEDCatch2)
+            FetchContent_Declare(fetchedCatch2
+                GIT_REPOSITORY ${openPMD_catch_repo}
+                GIT_TAG        ${openPMD_catch_branch}
+                BUILD_IN_SOURCE OFF
+            )
         endif()
+        FetchContent_MakeAvailable(fetchedCatch2)
+
+        # advanced fetch options
+        mark_as_advanced(FETCHCONTENT_BASE_DIR)
+        mark_as_advanced(FETCHCONTENT_FULLY_DISCONNECTED)
+        mark_as_advanced(FETCHCONTENT_QUIET)
+        #mark_as_advanced(FETCHCONTENT_SOURCE_DIR_FETCHEDCatch2)
+        mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED)
+        #mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED_FETCHEDCatch2)
     elseif(NOT openPMD_USE_INTERNAL_CATCH)
         find_package(Catch2 2.13.10 CONFIG REQUIRED)
         message(STATUS "Catch2: Found version '${Catch2_VERSION}'")
--- cmake/dependencies/pybind11.cmake
+++ cmake/dependencies/pybind11.cmake
@@ -1,49 +1,49 @@
 function(find_pybind11)
     if(TARGET pybind11::module)
         message(STATUS "pybind11::module target already imported")
-    elseif(openPMD_pybind11_src)
-        message(STATUS "Compiling local pybind11 ...")
-        message(STATUS "pybind11 source path: ${openPMD_pybind11_src}")
-        if(NOT IS_DIRECTORY ${openPMD_pybind11_src})
-            message(FATAL_ERROR "Specified directory openPMD_pybind11_src='${openPMD_pybind11_src}' does not exist!")
-        endif()
-    elseif(openPMD_pybind11_tar)
-        message(STATUS "Downloading pybind11 ...")
-        message(STATUS "pybind11 source: ${openPMD_pybind11_tar}")
     elseif(openPMD_USE_INTERNAL_PYBIND11)
-        message(STATUS "Downloading pybind11 ...")
-        message(STATUS "pybind11 repository: ${openPMD_pybind11_repo} (${openPMD_pybind11_branch})")
+        if(openPMD_pybind11_src)
+            message(STATUS "Compiling local pybind11 ...")
+            message(STATUS "pybind11 source path: ${openPMD_pybind11_src}")
+            if(NOT IS_DIRECTORY ${openPMD_pybind11_src})
+                message(FATAL_ERROR "Specified directory openPMD_pybind11_src='${openPMD_pybind11_src}' does not exist!")
+            endif()
+        elseif(openPMD_pybind11_tar)
+            message(STATUS "Downloading pybind11 ...")
+            message(STATUS "pybind11 source: ${openPMD_pybind11_tar}")
+        elseif(openPMD_pybind11_branch)
+            message(STATUS "Downloading pybind11 ...")
+            message(STATUS "pybind11 repository: ${openPMD_pybind11_repo} (${openPMD_pybind11_branch})")
+        endif()
     endif()
     if(TARGET pybind11::module)
         # nothing to do, target already exists in the superbuild
-    elseif(openPMD_USE_INTERNAL_PYBIND11 OR openPMD_pybind11_src)
-        if(openPMD_pybind11_src)
-            add_subdirectory(${openPMD_pybind11_src} _deps/localpybind11-build/)
-        else()
-            include(FetchContent)
-            if(openPMD_pybind11_tar)
-                FetchContent_Declare(fetchedpybind11
-                        URL             ${openPMD_pybind11_tar}
-                        URL_HASH        ${openPMD_pybind11_tar_hash}
-                        BUILD_IN_SOURCE OFF
-                )
-            else()
-                FetchContent_Declare(fetchedpybind11
-                    GIT_REPOSITORY ${openPMD_pybind11_repo}
-                    GIT_TAG        ${openPMD_pybind11_branch}
+    elseif(openPMD_USE_INTERNAL_PYBIND11 AND openPMD_pybind11_src)
+        add_subdirectory(${openPMD_pybind11_src} _deps/localpybind11-build/)
+    elseif(openPMD_USE_INTERNAL_PYBIND11 AND (openPMD_pybind11_tar OR openPMD_pybind11_branch))
+        include(FetchContent)
+        if(openPMD_pybind11_tar)
+            FetchContent_Declare(fetchedpybind11
+                    URL             ${openPMD_pybind11_tar}
+                    URL_HASH        ${openPMD_pybind11_tar_hash}
                     BUILD_IN_SOURCE OFF
-                )
-            endif()
-            FetchContent_MakeAvailable(fetchedpybind11)
-
-            # advanced fetch options
-            mark_as_advanced(FETCHCONTENT_BASE_DIR)
-            mark_as_advanced(FETCHCONTENT_FULLY_DISCONNECTED)
-            mark_as_advanced(FETCHCONTENT_QUIET)
-            mark_as_advanced(FETCHCONTENT_SOURCE_DIR_FETCHEDpybind11)
-            mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED)
-            mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED_FETCHEDpybind11)
+            )
+        else()
+            FetchContent_Declare(fetchedpybind11
+                GIT_REPOSITORY ${openPMD_pybind11_repo}
+                GIT_TAG        ${openPMD_pybind11_branch}
+                BUILD_IN_SOURCE OFF
+            )
         endif()
+        FetchContent_MakeAvailable(fetchedpybind11)
+
+        # advanced fetch options
+        mark_as_advanced(FETCHCONTENT_BASE_DIR)
+        mark_as_advanced(FETCHCONTENT_FULLY_DISCONNECTED)
+        mark_as_advanced(FETCHCONTENT_QUIET)
+        mark_as_advanced(FETCHCONTENT_SOURCE_DIR_FETCHEDpybind11)
+        mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED)
+        mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED_FETCHEDpybind11)
     elseif(NOT openPMD_USE_INTERNAL_PYBIND11)
         if(openPMD_USE_PYTHON STREQUAL AUTO)
             find_package(pybind11 2.13.0 CONFIG)
