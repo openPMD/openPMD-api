9e2053127d67df1e06b88443c9f2584c24b6787d
Franz Pöschel <franz.poeschel@gmail.com>
2024-11-08 11:41:15 +0100

    Take TOML out of Python testing

--- test/python/unittest/API/APITest.py
+++ test/python/unittest/API/APITest.py
@@ -25,8 +25,10 @@ except ImportError:
 from TestUtilities.TestUtilities import generateTestFilePath
 
 tested_file_extensions = [
-    ext for ext in io.file_extensions if ext != 'sst' and ext != 'ssc'
-]
+    ext for ext in io.file_extensions
+    # TOML is relatively slow and it's just an adaptor for the JSON backend,
+    # so it doesn't require full testing
+    if ext !='sst' and ext !='ssc' and ext !='toml']
 
 
 class APITest(unittest.TestCase):
