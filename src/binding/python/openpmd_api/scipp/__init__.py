69dd93b06d5bd4dc2ac2ad7537df7a46d290c43d
Paweł Ordyna <p.ordyna@hzdr.de>
2025-01-31 17:54:36 +0100

    Add Code from openpmd_scipp repository
    https://github.com/pordyna/openpmd_scipp

---
+++ src/binding/python/openpmd_api/scipp/__init__.py
@@ -0,0 +1,13 @@
+"""openpmd_scipp: A Python package for loading openPMD datasets into scipp DataArrays.
+
+See README.md for documentation
+
+Author:
+    Pawel Ordyna <p.ordyna@hzdr.de>
+
+License:
+GPL - 3.0 license. See LICENSE file for details.
+"""
+
+from .loader import DataLoader as DataLoader
+from .utils import closest as closest
