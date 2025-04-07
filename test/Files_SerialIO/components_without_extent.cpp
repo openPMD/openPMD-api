84b8d0a51dfd72738f14ea5b9ab3e66f29266d56
Franz Pöschel <franz.poeschel@gmail.com>
2025-04-07 18:10:20 +0200

    Extend test

--- test/Files_SerialIO/components_without_extent.cpp
+++ test/Files_SerialIO/components_without_extent.cpp
@@ -9,9 +9,10 @@
 
 namespace components_without_extent
 {
-auto components_without_extent() -> void
+constexpr char const *filepath = "../samples/components_without_extent.bp5";
+
+void particle_offset_without_extent()
 {
-    auto filepath = "../samples/components_without_extent.bp5";
     // write
     {
         openPMD::Series write(filepath, openPMD::Access::CREATE);
@@ -44,5 +45,214 @@ auto components_without_extent() -> void
             }
         }
     }
+}
+
+void particles_without_any_extent()
+{
+    // write
+    {
+        openPMD::Series write(filepath, openPMD::Access::CREATE);
+        auto it0 = write.writeIterations()[0];
+        auto e = it0.particles["e"];
+        for (auto comp_id : {"x", "y", "z"})
+        {
+            auto position_comp = e["position"][comp_id];
+            position_comp.resetDataset({openPMD::Datatype::INT, {}});
+            position_comp.makeConstant(0);
+
+            auto offset_comp = e["positionOffset"][comp_id];
+            offset_comp.resetDataset({openPMD::Datatype::INT, {}});
+            offset_comp.makeConstant(0);
+        }
+        write.close();
+    }
+
+    // read
+    {
+        openPMD::Series read(filepath, openPMD::Access::READ_RANDOM_ACCESS);
+        REQUIRE(!read.snapshots()[0].particles.contains("e"));
+    }
+
+    {
+        openPMD::Series read(
+            filepath,
+            openPMD::Access::READ_RANDOM_ACCESS,
+            R"({"verify_homogeneous_extents": false})");
+        REQUIRE(read.snapshots()[0].particles.contains("e"));
+        auto e = read.snapshots()[0].particles["e"];
+        for (auto const &record : e)
+        {
+            for (auto const &component : record.second)
+            {
+                REQUIRE(component.second.getExtent() == openPMD::Extent{});
+            }
+        }
+    }
+}
+
+void particles_without_inconsistent_extent()
+{
+    // write
+    {
+        openPMD::Series write(filepath, openPMD::Access::CREATE);
+        auto it0 = write.writeIterations()[0];
+        auto e = it0.particles["e"];
+        for (auto comp_id : {"x", "y", "z"})
+        {
+            auto position_comp = e["position"][comp_id];
+            position_comp.resetDataset({openPMD::Datatype::INT, {5}});
+            position_comp.makeConstant(0);
+
+            auto offset_comp = e["positionOffset"][comp_id];
+            offset_comp.resetDataset({openPMD::Datatype::INT, {10}});
+            offset_comp.makeConstant(0);
+        }
+        write.close();
+    }
+
+    // read
+    {
+        openPMD::Series read(filepath, openPMD::Access::READ_RANDOM_ACCESS);
+        REQUIRE(!read.snapshots()[0].particles.contains("e"));
+    }
+
+    {
+        openPMD::Series read(
+            filepath,
+            openPMD::Access::READ_RANDOM_ACCESS,
+            R"({"verify_homogeneous_extents": false})");
+        REQUIRE(read.snapshots()[0].particles.contains("e"));
+        auto e = read.snapshots()[0].particles["e"];
+        for (auto const &component : e["position"])
+        {
+            REQUIRE(component.second.getExtent() == openPMD::Extent{5});
+        }
+        for (auto const &component : e["positionOffset"])
+        {
+            REQUIRE(component.second.getExtent() == openPMD::Extent{10});
+        }
+    }
+}
+
+void meshes_with_incomplete_extent()
+{
+    // write
+    {
+        openPMD::Series write(filepath, openPMD::Access::CREATE);
+        auto it0 = write.writeIterations()[0];
+        auto E = it0.meshes["E"];
+        for (auto comp_id : {"x"})
+        {
+            auto comp = E[comp_id];
+            comp.resetDataset({openPMD::Datatype::FLOAT, {5}});
+            std::unique_ptr<float[]> data{new float[5]};
+            std::iota(data.get(), data.get() + 5, 0);
+            comp.storeChunk(std::move(data), {0}, {5});
+        }
+        for (auto comp_id : {"y", "z"})
+        {
+            auto comp = E[comp_id];
+            comp.resetDataset({openPMD::Datatype::INT, {}});
+            comp.makeConstant(0);
+        }
+        write.close();
+    }
+
+    // read
+    {
+        openPMD::Series read(filepath, openPMD::Access::READ_RANDOM_ACCESS);
+        auto E = read.snapshots()[0].meshes["E"];
+        for (auto const &component : E)
+        {
+            REQUIRE(component.second.getExtent() == openPMD::Extent{5});
+        }
+    }
+}
+
+void meshes_with_inconsistent_extent()
+{
+    // write
+    {
+        openPMD::Series write(filepath, openPMD::Access::CREATE);
+        auto it0 = write.writeIterations()[0];
+        auto E = it0.meshes["E"];
+        size_t i = 1;
+        for (auto comp_id : {"x", "y", "z"})
+        {
+            size_t extent = i++ * 5;
+            auto comp = E[comp_id];
+            comp.resetDataset({openPMD::Datatype::FLOAT, {extent}});
+            std::unique_ptr<float[]> data{new float[extent]};
+            std::iota(data.get(), data.get() + extent, 0);
+            comp.storeChunk(std::move(data), {0}, {extent});
+        }
+        write.close();
+    }
+
+    // read
+    {
+        openPMD::Series read(filepath, openPMD::Access::READ_RANDOM_ACCESS);
+        REQUIRE(!read.snapshots()[0].meshes.contains("E"));
+    }
+
+    // read
+    {
+        openPMD::Series read(
+            filepath,
+            openPMD::Access::READ_RANDOM_ACCESS,
+            R"({"verify_homogeneous_extents": false})");
+        auto E = read.snapshots()[0].meshes["E"];
+        size_t i = 1;
+        for (auto const &component : E)
+        {
+            REQUIRE(component.second.getExtent() == openPMD::Extent{5 * i++});
+        }
+    }
+}
+
+void meshes_without_any_extent()
+{
+    // write
+    {
+        openPMD::Series write(filepath, openPMD::Access::CREATE);
+        auto it0 = write.writeIterations()[0];
+        auto E = it0.meshes["E"];
+        for (auto comp_id : {"x", "y", "z"})
+        {
+            auto comp = E[comp_id];
+            comp.resetDataset({openPMD::Datatype::FLOAT, {}});
+            comp.makeConstant<float>(0);
+        }
+        write.close();
+    }
+
+    // read
+    {
+        openPMD::Series read(filepath, openPMD::Access::READ_RANDOM_ACCESS);
+        REQUIRE(!read.snapshots()[0].meshes.contains("E"));
+    }
+
+    // read
+    {
+        openPMD::Series read(
+            filepath,
+            openPMD::Access::READ_RANDOM_ACCESS,
+            R"({"verify_homogeneous_extents": false})");
+        auto E = read.snapshots()[0].meshes["E"];
+        for (auto const &component : E)
+        {
+            REQUIRE(component.second.getExtent() == openPMD::Extent{});
+        }
+    }
+}
+
+auto components_without_extent() -> void
+{
+    particle_offset_without_extent();
+    particles_without_any_extent();
+    particles_without_inconsistent_extent();
+    meshes_with_incomplete_extent();
+    meshes_with_inconsistent_extent();
+    meshes_without_any_extent();
 }
 } // namespace components_without_extent
