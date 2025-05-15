83303f607e9137da7b52b226dbcf2d842b98c633
Franz Pöschel <franz.poeschel@gmail.com>
2025-05-15 16:18:26 +0200

    Update cmake/dependencies/toml11.cmake

--- cmake/dependencies/toml11.cmake
+++ cmake/dependencies/toml11.cmake
@@ -47,7 +47,6 @@ function(find_toml11)
         #mark_as_advanced(FETCHCONTENT_SOURCE_DIR_FETCHEDtoml11)
         mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED)
         #mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED_FETCHEDtoml11)
-
     elseif(NOT openPMD_USE_INTERNAL_TOML11)
         # toml11 4.0 was a breaking change. This is reflected in the library's CMake
         # logic: version 4.0 is not accepted by a call to find_package(toml11 3.7).
