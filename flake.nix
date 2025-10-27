84e0193f8b1b42bdb24a6c0901c4f21119179cf1
Franz Pöschel <franz.poeschel@gmail.com>
2025-10-27 17:24:58 +0100

    Tests
    MPI test failing still

--- flake.nix
+++ flake.nix
@@ -62,55 +62,16 @@
       #     #systemd.services = { ... };
       #   };
 
-      # # Tests run by 'nix flake check' and by Hydra.
-      # checks = forAllSystems
-      #   (system:
-      #     with nixpkgsFor.${system};
+      # Tests run by 'nix flake check' and by Hydra.
+      checks = forAllSystems
+        (system:
+          with self.packages.${system};
 
-      #     {
-      #       inherit (self.packages.${system}) hello;
-
-      #       # Additional tests, if applicable.
-      #       test = stdenv.mkDerivation {
-      #         pname = "hello-test";
-      #         inherit version;
-
-      #         buildInputs = [ hello ];
-
-      #         dontUnpack = true;
-
-      #         buildPhase = ''
-      #           echo 'running some integration tests'
-      #           [[ $(hello) = 'Hello Nixers!' ]]
-      #         '';
-
-      #         installPhase = "mkdir -p $out";
-      #       };
-      #     }
-
-      #     // lib.optionalAttrs stdenv.isLinux {
-      #       # A VM test of the NixOS module.
-      #       vmTest =
-      #         with import (nixpkgs + "/nixos/lib/testing-python.nix") {
-      #           inherit system;
-      #         };
-
-      #         makeTest {
-      #           nodes = {
-      #             client = { ... }: {
-      #               imports = [ self.nixosModules.hello ];
-      #             };
-      #           };
-
-      #           testScript =
-      #             ''
-      #               start_all()
-      #               client.wait_for_unit("multi-user.target")
-      #               client.succeed("hello")
-      #             '';
-      #         };
-      #     }
-      #   );
+          {
+            # Additional tests, if applicable.
+            test = openpmd_api.override { doCheck = true; };
+          }
+        );
 
     };
 }
--- nix/openpmd_api/default.nix
+++ nix/openpmd_api/default.nix
@@ -9,10 +9,11 @@
 , adios2 ? null
 , mpi ? null
 , python ? null
+, doCheck ? false
 }:
 
 let
-  cmake_on_off = dependency: if dependency == null then "OFF" else "ON";
+  cmake_on_off = dependency: if dependency == null || dependency == false then "OFF" else "ON";
   pybind11 = python.pkgs.pybind11 or null;
 
   # for key `format`
@@ -29,15 +30,24 @@ let
 
 in
 builder (builder_params // rec {
-  inherit version;
+  inherit version doCheck;
   pname = "openPMD-api";
 
   src = ../..;
 
   preConfigure = ''
     # wont find these three otherwise
-    export CMAKE_PREFIX_PATH="${maybe_path toml11}${maybe_path nlohmann_json}${maybe_path pybind11}$CMAKE_PREFIX_PATH"
+    export CMAKE_PREFIX_PATH="${maybe_path catch2}${maybe_path toml11}${maybe_path nlohmann_json}${maybe_path pybind11}$CMAKE_PREFIX_PATH"
+    ${if doCheck then ''
+      # TODO: download samples
+    '' else ""}
   '';
+
+  postBuild =
+    if doCheck then ''
+      ctest --output-on-failure
+      false
+    '' else "";
 
   postInstall = ''
     sed -Ei 's|=.*}/'"$out"'|='"$out"'|' $out/lib/pkgconfig/openPMD.pc
@@ -70,8 +80,8 @@ builder (builder_params // rec {
     ]);
 
   cmakeFlags = [
-    "-DopenPMD_BUILD_TESTING=OFF"
-    "-DopenPMD_BUILD_EXAMPLES=OFF"
+    "-DopenPMD_BUILD_TESTING=${cmake_on_off doCheck}"
+    "-DopenPMD_BUILD_EXAMPLES=${cmake_on_off doCheck}"
     "-DopenPMD_USE_ADIOS2=${cmake_on_off adios2}"
     "-DopenPMD_USE_HDF5=${cmake_on_off hdf5}"
     "-DopenPMD_USE_MPI=${cmake_on_off mpi}"
@@ -85,6 +95,8 @@ builder (builder_params // rec {
   ++ (if mpi != null && python != null then [
     "-DCMAKE_C_COMPILER=${mpi.dev}/bin/mpicc"
     "-DCMAKE_CXX_COMPILER=${mpi.dev}/bin/mpicxx"
+  ] else [ ]) ++ (if mpi != null && doCheck then [
+    "-DMPIEXEC_EXECUTABLE=${mpi}/bin/mpiexec"
   ] else [ ]);
 
   meta = {
