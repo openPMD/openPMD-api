c4f9f0763473fc05c442383d56840ddb61133624
Franz Pöschel <franz.poeschel@gmail.com>
2026-01-23 17:44:43 +0100

    more paths

--- library_builders.bat
+++ library_builders.bat
@@ -32,7 +32,7 @@ exit /b 0
   git config user.name "Tooling"
   git add .
   git commit --message="Initial commit so we can use git-am"
-  git am ..\..\0001-Fix-h_errno-declarations-for-Windows.patch
+  git am ..\..\..\..\0001-Fix-h_errno-declarations-for-Windows.patch
   git show
   cd ..
   cd ..
