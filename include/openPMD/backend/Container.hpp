498e9dac6b34bbd28b9d563ef82d50f97122db3d
Franz Pöschel <franz.poeschel@gmail.com>
2026-03-09 12:24:50 +0100

    Erase Iteration upon close (write mode)

--- include/openPMD/backend/Container.hpp
+++ include/openPMD/backend/Container.hpp
@@ -114,6 +114,7 @@ class Container : virtual public Attributable
     template <typename>
     friend class internal::EraseStaleEntries;
     friend class StatefulIterator;
+    friend class StatefulSnapshotsContainer;
 
 protected:
     using ContainerData = internal::ContainerData<T, T_key, T_container>;
