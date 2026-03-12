931ac106a95ec2894dce6560f6329a6293c1203a
Franz Pöschel <franz.poeschel@gmail.com>
2026-03-12 12:52:17 +0100

    Hmm, move CREATE_DATASET task back to storeChunk
    not so great, but lets keep that for now

--- src/RecordComponent.cpp
+++ src/RecordComponent.cpp
@@ -243,12 +243,8 @@ RecordComponent &RecordComponent::resetDataset(Dataset d)
             rc.m_dataset->dtype != Datatype::UNDEFINED &&
             IOHandler()->m_seriesStatus != internal::SeriesStatus::Parsing)
         {
-            // TODO: try getting flush_io_handler = false to run
             seriesFlush_impl</* flush_entire_series = */ false>(
                 {FlushLevel::SkeletonOnly}, /* flush_io_handler = */ true);
-            Parameter<Operation::CREATE_DATASET> dCreate(rc.m_dataset.value());
-            dCreate.name = Attributable::get().m_writable.ownKeyWithinParent;
-            IOHandler()->enqueue(IOTask(this, dCreate));
         }
     });
     if (written())
