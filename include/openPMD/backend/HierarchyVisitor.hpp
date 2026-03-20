97cf73349df9f6dd7daed9bf86c3079387b80b81
Franz Pöschel <franz.poeschel@gmail.com>
2026-03-20 19:05:16 +0100

    Further cleanup

--- include/openPMD/backend/HierarchyVisitor.hpp
+++ include/openPMD/backend/HierarchyVisitor.hpp
@@ -6,45 +6,34 @@
 
 namespace openPMD
 {
-class Series;
 class Iteration;
+class Iterations;
 class Mesh;
-class ParticleSpecies;
+class MeshRecordComponent;
+class Meshes;
 class ParticlePatches;
+class ParticleSpecies;
+class Particles;
 class PatchRecord;
 class PatchRecordComponent;
-class RecordComponent;
-class MeshRecordComponent;
-template <typename, typename, typename>
-class Container;
-template <typename Val, typename Key = std::string>
-using Cont = Container<Val, Key, std::map<Key, Val>>;
-template <typename>
-class BaseRecord;
 class Record;
+class RecordComponent;
+class Series;
 
 class HierarchyVisitor
 {
 public:
-    virtual void operator()(Series &) = 0;
     virtual void operator()(Iteration &) = 0;
-    virtual void operator()(Cont<Iteration, std::uint64_t> &) = 0;
-    virtual void operator()(Cont<Mesh> &) = 0;
-    virtual void operator()(Cont<ParticleSpecies> &) = 0;
-    virtual void operator()(Cont<ParticlePatches> &) = 0;
-    virtual void operator()(Cont<PatchRecord> &) = 0;
-    virtual void operator()(Cont<Record> &) = 0;
-    virtual void operator()(Record &) = 0;
-    // TODO there are too many duplications here, remove most of them
-    virtual void operator()(Cont<MeshRecordComponent> &) = 0;
-    virtual void operator()(BaseRecord<MeshRecordComponent> &) = 0;
-    virtual void operator()(Cont<PatchRecordComponent> &) = 0;
-    virtual void operator()(BaseRecord<PatchRecordComponent> &) = 0;
-    virtual void operator()(Cont<RecordComponent> &) = 0;
-    virtual void operator()(BaseRecord<RecordComponent> &) = 0;
-    virtual void operator()(ParticleSpecies &) = 0;
-    virtual void operator()(RecordComponent &) = 0;
+    virtual void operator()(Iterations &) = 0;
+    virtual void operator()(Mesh &) = 0;
     virtual void operator()(MeshRecordComponent &) = 0;
+    virtual void operator()(Meshes &) = 0;
+    virtual void operator()(ParticlePatches &) = 0;
+    virtual void operator()(ParticleSpecies &) = 0;
+    virtual void operator()(Particles &) = 0;
     virtual void operator()(PatchRecordComponent &) = 0;
+    virtual void operator()(Record &) = 0;
+    virtual void operator()(RecordComponent &) = 0;
+    virtual void operator()(Series &) = 0;
 };
 } // namespace openPMD
