8587e97fa5d7c8c502ef06dfb3426f5c5271677e
Franz Pöschel <franz.poeschel@gmail.com>
2026-08-03 17:51:37 +0200

    Fix tuple length verification

--- include/openPMD/binding/python/Pickle.hpp
+++ include/openPMD/binding/python/Pickle.hpp
@@ -128,7 +128,7 @@ add_pickle(pybind11::class_<T_Args...> &cl, T_SeriesAccessor &&seriesAccessor)
             // __setstate__
             [&seriesAccessor](py::tuple const &t) {
                 // our tuple has exactly two elements: filePath & group
-                if (t.size() != 2)
+                if (t.size() != 3)
                     throw std::runtime_error("Invalid state!");
 
                 auto id = t[0].cast<uintptr_t>();
