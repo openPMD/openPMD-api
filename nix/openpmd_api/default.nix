b572f9d3dd827b8d8c604c680e4256d16427c16e
Blablador <noreply@blablador.localhost>
2026-09-04 11:52:07 +0200

    nix: use upstream nixpkgs adios2 and pybind11, fix metadata
    Drop the hand-maintained ADIOS2 fork and the pybind11/python3 override
    machinery in favor of the nixpkgs-provided packages, which already
    satisfy the version requirements. Clean up the openpmd_api package:
    
    - propagate mpi only once instead of both mpi and mpi.dev
    - use lib.cmakeBool and lowercase pname
    - set proper meta (homepage, description, changelog, license,
      maintainers) and build for all of platforms.unix instead of x86
      only, fixing builds on aarch64

--- nix/openpmd_api/default.nix
+++ nix/openpmd_api/default.nix
@@ -1,112 +1,140 @@
-{ lib
-, stdenv
-, version
-, cmake
-, nlohmann_json
-, toml11
-, catch2
-, hdf5 ? null
-, adios2 ? null
-, mpi ? null
-, python ? null
-, doCheck ? false
+{
+  lib,
+  stdenv,
+  version,
+  cmake,
+  nlohmann_json,
+  toml11,
+  catch2,
+  hdf5 ? null,
+  adios2 ? null,
+  mpi ? null,
+  python ? null,
+  doCheck ? false,
 }:
 
 let
-  cmake_on_off = dependency: if dependency == null || dependency == false then "OFF" else "ON";
   pybind11 = python.pkgs.pybind11 or null;
 
   # for key `format`
   # pkgs/development/interpreters/python/mk-python-derivation.nix
-  builder =
-    if python != null
-    then python.pkgs.buildPythonPackage
-    else stdenv.mkDerivation;
-  builder_params =
-    if python != null
-    then { format = "other"; }
-    else { };
+  builder = if python != null then python.pkgs.buildPythonPackage else stdenv.mkDerivation;
+  builder_params = if python != null then { format = "other"; } else { };
   maybe_path = dependency: lib.optionalString (dependency != null) "${dependency}:";
 
 in
-builder (builder_params // rec {
-  inherit version doCheck;
-  pname = "openPMD-api";
+builder (
+  builder_params
+  // rec {
+    inherit version doCheck;
+    pname = "openpmd-api";
 
-  src = ../..;
+    src = ../..;
 
-  preConfigure = ''
-    # wont find these three otherwise
-    export CMAKE_PREFIX_PATH="${maybe_path catch2}${maybe_path toml11}${maybe_path nlohmann_json}${maybe_path pybind11}$CMAKE_PREFIX_PATH"
-    ${if doCheck then ''
-      # TODO: download samples
-    '' else ""}
-  '';
+    preConfigure = ''
+      # wont find these three otherwise
+      export CMAKE_PREFIX_PATH="${maybe_path catch2}${maybe_path toml11}${maybe_path nlohmann_json}${maybe_path pybind11}$CMAKE_PREFIX_PATH"
+      ${
+        if doCheck then
+          ''
+            # TODO: download samples
+          ''
+        else
+          ""
+      }
+    '';
 
-  postBuild =
-    if doCheck then ''
-      ctest --output-on-failure
-      false
-    '' else "";
+    postBuild =
+      if doCheck then
+        ''
+          ctest --output-on-failure
+          false
+        ''
+      else
+        "";
 
-  postInstall = ''
-    sed -Ei 's|=.*}/'"$out"'|='"$out"'|' $out/lib/pkgconfig/openPMD.pc
-  '';
+    postInstall = ''
+      sed -Ei 's|=.*}/'"$out"'|='"$out"'|' $out/lib/pkgconfig/openPMD.pc
+    '';
 
-  propagatedBuildInputs =
-    builtins.filter
-      (el: el != null)
-      ([
+    propagatedBuildInputs =
+      builtins.filter (el: el != null) [
         hdf5
         adios2
         mpi
-      ] ++ lib.optional (mpi != null) mpi.dev)
-    ++
-    (if python != null
-    then [
-      python
-      python.pkgs.mpi4py
-      python.pkgs.numpy
+      ]
+      ++ (
+        if python != null then
+          [
+            python
+            python.pkgs.mpi4py
+            python.pkgs.numpy
+          ]
+        else
+          [ ]
+      );
+    nativeBuildInputs = (
+      builtins.filter (el: el != null) [
+        toml11
+        nlohmann_json
+        cmake
+        catch2
+        pybind11
+      ]
+    );
+
+    cmakeFlags = [
+      (lib.cmakeBool "openPMD_BUILD_TESTING" doCheck)
+      (lib.cmakeBool "openPMD_BUILD_EXAMPLES" doCheck)
+      (lib.cmakeBool "openPMD_USE_ADIOS2" (adios2 != null))
+      (lib.cmakeBool "openPMD_USE_HDF5" (hdf5 != null))
+      (lib.cmakeBool "openPMD_USE_MPI" (mpi != null))
+      (lib.cmakeBool "openPMD_USE_PYTHON" (python != null))
+      (lib.cmakeBool "openPMD_USE_INTERNAL_CATCH" false)
+      (lib.cmakeBool "openPMD_USE_INTERNAL_JSON" false)
+      (lib.cmakeBool "openPMD_USE_INTERNAL_TOML11" false)
+      (lib.cmakeBool "openPMD_USE_INTERNAL_PYBIND11" false)
     ]
-    else [ ]);
-  nativeBuildInputs =
-    (builtins.filter (el: el != null) [
-      toml11
-      nlohmann_json
-      cmake
-      catch2
-      pybind11
-    ]);
+    # buildPythonPackage might have issues here
+    ++ (
+      if mpi != null && python != null then
+        [
+          "-DCMAKE_C_COMPILER=${mpi.dev}/bin/mpicc"
+          "-DCMAKE_CXX_COMPILER=${mpi.dev}/bin/mpicxx"
+        ]
+      else
+        [ ]
+    )
+    ++ (
+      if mpi != null && doCheck then
+        [
+          "-DMPIEXEC_EXECUTABLE=${mpi}/bin/mpiexec"
+        ]
+      else
+        [ ]
+    );
 
-  cmakeFlags = [
-    "-DopenPMD_BUILD_TESTING=${cmake_on_off doCheck}"
-    "-DopenPMD_BUILD_EXAMPLES=${cmake_on_off doCheck}"
-    "-DopenPMD_USE_ADIOS2=${cmake_on_off adios2}"
-    "-DopenPMD_USE_HDF5=${cmake_on_off hdf5}"
-    "-DopenPMD_USE_MPI=${cmake_on_off mpi}"
-    "-DopenPMD_USE_PYTHON=${cmake_on_off python}"
-    "-DopenPMD_USE_INTERNAL_CATCH=OFF"
-    "-DopenPMD_USE_INTERNAL_JSON=OFF"
-    "-DopenPMD_USE_INTERNAL_TOML11=OFF"
-    "-DopenPMD_USE_INTERNAL_PYBIND11=OFF"
-  ]
-  # buildPythonPackage might have issues here
-  ++ (if mpi != null && python != null then [
-    "-DCMAKE_C_COMPILER=${mpi.dev}/bin/mpicc"
-    "-DCMAKE_CXX_COMPILER=${mpi.dev}/bin/mpicxx"
-  ] else [ ]) ++ (if mpi != null && doCheck then [
-    "-DMPIEXEC_EXECUTABLE=${mpi}/bin/mpiexec"
-  ] else [ ]);
-
-  meta = {
-    # homepage = "https://rr-project.org/";
-    description = "openPMD-api";
-    longDescription = ''
-      openPMD-api
-    '';
-
-    license = with lib.licenses; [ lgpl3 ];
-    platforms = lib.platforms.x86;
-  };
-}
+    meta = with lib; {
+      homepage = "https://github.com/openPMD/openPMD-api";
+      description = "C++ & Python API for scientific I/O with openPMD";
+      longDescription = ''
+        The openPMD-api provides a high-level C++ and Python interface for
+        reading and writing data following the openPMD standard, supporting
+        the HDF5 and ADIOS2 file formats.
+      '';
+      changelog = "https://github.com/openPMD/openPMD-api/blob/dev/CHANGELOG.rst";
+      license = licenses.lgpl3Plus;
+      platforms = platforms.unix;
+      maintainers = [
+        {
+          name = "Axel Huebl";
+          github = "ax3l";
+        }
+        {
+          name = "Franz Pöschel";
+          github = "franzpoeschel";
+        }
+      ];
+    };
+  }
 )
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -2,33 +2,13 @@
 final: prev:
 let
   callPackage = final.callPackage;
-
-  pybind11 = import ./pybind11 { fetchFromGitHub = final.fetchFromGitHub; };
-
-  python3 = import ./python3 {
-    inherit (prev) lib;
-    python = prev.python3;
-    packageOverlays = final.pythonPackagesOverlays;
-  };
-
 in
 {
-  adios2 = callPackage ./adios2 {
-    python = final.python3;
-  };
   openpmd_api = callPackage ./openpmd_api {
     inherit version;
-    python = final.python3;
+    python = prev.python3;
     hdf5 = final.hdf5-mpi;
+    adios2 = prev.adios2;
     catch2 = final.catch2_3;
   };
-
-  # python overlay as in
-  # https://discourse.nixos.org/t/add-python-package-via-overlay/19783/3
-  pythonPackagesOverlays = (prev.pythonPackagesOverlays or [ ]) ++ [
-    pybind11
-  ];
-
-  inherit python3;
-  python3Packages = final.python3.pkgs;
 }
